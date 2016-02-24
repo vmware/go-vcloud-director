4ca0ed1ad798e75496482d3e03c265b9c9c56bfe
Rob Coward <rob@jive-videos.net>
2016-02-24 11:14:26 +0000

    Removed debugging.

--- query.go
+++ query.go
@@ -6,8 +6,6 @@ package govcd
 
 import (
 	"fmt"
-	// "net/http/httputil"
-	"log"
 
 	types "github.com/hmrc/vmware-govcd/types/v56"
 )
@@ -24,25 +22,15 @@ func NewResults(c *Client) *Results {
 	}
 }
 
-// func debug(data []byte, err error) {
-//     if err == nil {
-//         fmt.Printf("%s\n\n", data)
-//     } else {
-//         log.Fatalf("%s\n\n", err)
-//     }
-// }
-
 func (c *VCDClient) Query(params map[string]string) (Results, error) {
 
 	req := c.Client.NewRequest(params, "GET", c.QueryHREF, nil)
 	req.Header.Add("Accept", "vnd.vmware.vcloud.org+xml;version=5.5")
-	// debug(httputil.DumpRequestOut(req, true))
 
 	resp, err := checkResp(c.Client.Http.Do(req))
 	if err != nil {
 		return Results{}, fmt.Errorf("error retreiving query: %s", err)
 	}
-	// debug(httputil.DumpResponse(resp, true))
 
 	results := NewResults(&c.Client)
 
