3cf146f39378083dd4381e62de0a346bd8c9dc3f
auppunda <auppunda@gmail.com>
2018-08-06 15:07:10 -0700

    Added in functions to get vdc and orgs by name I added in get functions for vdcs and orgs. I added one to be able to get ParentVdc from a vapp, added another to be able to get a vdc from an org, and added a third to be able to get an Org object from a vcdClient connection. Also added in variable VCDHREF to the client, as oncewe remove our reliance on vcdvdchref, we will be using this for functions that need the api endpoint. Also added in tests for each of these functions
    Signed-off-by: auppunda <auppunda@gmail.com>

--- govcd/org.go
+++ govcd/org.go
@@ -23,6 +23,38 @@ func NewOrg(c *Client) *Org {
 	}
 }
 
+// If user specifies valid vdc name then this returns a vdc object.
+// Otherwise it returns an empty vdc and an error.
+func (o *Org) GetVdcByName(vdcname string) (Vdc, error) {
+	HREF := ""
+	for _, a := range o.Org.Link {
+		if a.Type == "application/vnd.vmware.vcloud.vdc+xml" && a.Name == vdcname {
+			HREF = a.HREF
+			break
+		}
+	}
+	if HREF == "" {
+		return Vdc{}, fmt.Errorf("Error finding VDC from VDCName")
+	}
+
+	u, err := url.ParseRequestURI(HREF)
+	if err != nil {
+		return Vdc{}, fmt.Errorf("Error parsing url: %v", err)
+	}
+	req := o.c.NewRequest(map[string]string{}, "GET", *u, nil)
+	resp, err := checkResp(o.c.Http.Do(req))
+	if err != nil {
+		return Vdc{}, fmt.Errorf("error getting vdc: %s", err)
+	}
+
+	vdc := NewVdc(o.c)
+	if err = decodeBody(resp, vdc.Vdc); err != nil {
+		return Vdc{}, fmt.Errorf("error decoding vdc response: %s", err)
+	}
+	// The request was successful
+	return *vdc, nil
+}
+
 func (o *Org) FindCatalog(catalog string) (Catalog, error) {
 
 	for _, av := range o.Org.Link {
