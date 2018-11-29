cf38cf584586444480ab0d2e2743ea2c78bed2f4
Vaidotas Bauzys <vbauzys@vmware.com>
2018-11-29 08:25:26 +0200

    Fix test
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- govcd/edgegateway_test.go
+++ govcd/edgegateway_test.go
@@ -56,7 +56,7 @@ func (vcd *TestVCD) Test_NATPortMapping(check *C) {
 	edge, err := vcd.vdc.FindEdgeGateway(vcd.config.VCD.EdgeGateway)
 	check.Assert(err, IsNil)
 	check.Assert(edge.EdgeGateway.Name, Equals, vcd.config.VCD.EdgeGateway)
-	task, err := edge.AddNATPortMapping("DNAT", vcd.config.VCD.ExternalIp, "1177", vcd.config.VCD.InternalIp, "77", "TCP", "any")
+	task, err := edge.AddNATPortMapping("DNAT", vcd.config.VCD.ExternalIp, "1177", vcd.config.VCD.InternalIp, "77", "TCP", "")
 	check.Assert(err, IsNil)
 	err = task.WaitTaskCompletion()
 	check.Assert(err, IsNil)
