cd9f56b5a7822a5d97fe508156084976479c670c
Giuseppe Maxia <gmaxia@vmware.com>
2019-01-02 11:12:29 +0100

    improve copyright check to better match file year
    The copyright check would fail if the year in the copyright line
    is not the current one. Changed the check to accept the previous year
    in the copyright, and only complain if the file was changed this year
    but the copyright notice uses the previous year.
    
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- scripts/copyright_check.sh
+++ scripts/copyright_check.sh
@@ -5,7 +5,10 @@
 vmware_any_copyright='Copyright \d\d\d\d VMware'
 
 # This check will find files with a copyright for the current year
-vmware_latest_copyright="Copyright $(date +%Y) VMware"
+this_year=$(date +%Y)
+last_year=$((this_year-1))
+vmware_latest_copyright="Copyright $this_year VMware"
+vmware_last_year_copyright="Copyright $last_year VMware"
 exit_code=0
 for F in $(find . -name '*.go' | grep -v '/vendor/' )
 do
@@ -15,12 +18,23 @@ do
         # Looks for copyright in the Nth line of the file
         has_any_copyright=$(head -n $line_num $F | tail -n 1 | grep "$vmware_any_copyright" )
         has_latest_copyright=$(head -n $line_num $F | tail -n 1 | grep "$vmware_latest_copyright" )
+        has_last_year_copyright=$(head -n $line_num $F | tail -n 1 | grep "$vmware_last_year_copyright" )
 
         if [ -n "$has_latest_copyright" ]
         then
             if [ "$1" == "-v" -o "$1" == "--verbose" ]
             then
                 echo "$F: latest copyright found in line $line_num"
+            fi
+            copyright_found=$line_num
+        elif [ -n "$has_last_year_copyright" ]
+        then
+            current_file_date=$(date -r $F)
+            updated_this_year=$(echo "$current_file_date" | grep -w $this_year)
+            if [ -n "$updated_this_year" ]
+            then
+                echo "$F updated this year, but has last year's copyright"
+                exit_code=1
             fi
             copyright_found=$line_num
         elif [ -n "$has_any_copyright" ]
