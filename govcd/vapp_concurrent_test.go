0a87f428dd935804398514b352d157bb3e8c0809
Dainius <Didainius@users.noreply.github.com>
2019-04-17 09:24:04 +0300

    Introduce race condition tests for VM and vApp (#181)
    * Introduce race condition tests for VM and vApp
    * Add 'make testconcurrent' option to run concurrent tests with race detector
    * Add a note about 'make testconcurrent' to TESTING.md

---
+++ govcd/vapp_concurrent_test.go
@@ -0,0 +1,35 @@
+// +build concurrent
+
+package govcd
+
+import (
+	"sync"
+
+	. "gopkg.in/check.v1"
+)
+
+// Test_VAPPRefreshConcurrent is meant to prove and show that structures across
+// go-vcloud-director are not thread-safe. Go tests must be run with -race flag
+// to capture race condition. It is also guarded by `concurrent` build tag and
+// is not run by default.
+//
+// Run with go test -race -tags "concurrent" -check.vv -check.f Test_VAPPRefreshConcurrent .
+func (vcd *TestVCD) Test_VAPPRefreshConcurrent(check *C) {
+	var waitgroup sync.WaitGroup
+
+	if vcd.skipVappTests {
+		check.Skip("Skipping test because vapp was not successfully created at setup")
+	}
+	vapp, err := vcd.vdc.FindVAppByName(vcd.vapp.VApp.Name)
+	check.Assert(err, IsNil)
+
+	for counter := 0; counter < 5; counter++ {
+		waitgroup.Add(1)
+		go func() {
+			_ = vapp.Refresh()
+			waitgroup.Done()
+			// check.Assert(err, IsNil)
+		}()
+	}
+	waitgroup.Wait()
+}
