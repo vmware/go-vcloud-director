ad7963bebcee42ab262bbae9e939769039899436
Vaidotas Bauzys <vbauzys@vmware.com>
2019-08-27 12:35:37 +0300

    Improve comment
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- govcd/vm_test.go
+++ govcd/vm_test.go
@@ -503,7 +503,7 @@ func (vcd *TestVCD) Test_InsertOrEjectMedia(check *C) {
 
 	// Skipping this test due to a bug in vCD. VM refresh status returns old state, though eject task is finished.
 	if vcd.client.APIVCDMaxVersionIs(">= 32.0, <= 33.0") {
-		check.Skip("Skipping test because this vCD version has bug")
+		check.Skip("Skipping test because this vCD version has a bug")
 	}
 
 	itemName := "TestInsertOrEjectMedia"
