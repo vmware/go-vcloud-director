e541ebbb4b177405e545bef659a304d2d239db6f
Giuseppe Maxia <gmaxia@vmware.com>
2020-07-07 09:49:25 +0200

    Add test for tag isolation

---
+++ scripts/test-tags.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# This test checks that all the build tags defined in api_vcd_test.go
+# can run individually
+
+if [ ! -d govcd ]
+then
+    echo "./govcvd directory missing"
+    exit 1
+fi
+cd govcd
+
+if [ ! -f api_vcd_test.go ]
+then
+    echo "file ./govcd/api_vcd_test.go not found"
+    exit 1
+fi
+
+start=$(date +%s)
+tags=$(head -n 1 api_vcd_test.go | sed -e 's/^.*build //')
+
+echo "=== RUN TagsTest"
+for tag in $tags
+do
+    
+    go test -tags $tag -timeout 0 -check.vv -vcd-help > /dev/null
+
+    if [ "$?" == "0" ]
+    then
+        echo "  --- PASS: TagsTest/$tag"
+    else
+        echo "  --- FAIL: TagsTest/$tag"
+        failed="$failed $tag"
+    fi
+done
+
+end=$(date +%s)
+elapsed=$((end-start))
+if [ -n "$failed" ]
+then
+    echo "--- FAIL: TagsTest - Tests for tags [$failed] have failed (${elapsed}s)"
+    exit 1
+fi
+echo "--- PASS: TagsTest (${elapsed}s)"
+exit 0
