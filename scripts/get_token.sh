699bd00cdf14e940b5aef38d7ea3a703de85c75a
Giuseppe Maxia <gmaxia@vmware.com>
2020-11-10 15:33:05 +0100

    Update script get_token.sh to switch version

--- scripts/get_token.sh
+++ scripts/get_token.sh
@@ -6,18 +6,35 @@ user=$1
 password=$2
 org=$3
 IP=$4
+api_version=$5
 
 if [ -z "$IP" ]
 then
-    echo "Syntax $0 user password organization hostname_or_IP_address"
+    echo "Syntax $0 user password organization hostname_or_IP_address [API version]"
     exit 1
 fi
 
 auth=$(echo -n "$user@$org:$password" |base64)
 
-curl -I -k --header "Accept: application/*;version=32.0" \
+[ -z "$api_version" ] && api_version=32
+operation=api/sessions
+
+# if the requested version is greater than 32 (VCD 10.0+), we can use cloudapi
+if [[ $api_version -ge 33  ]]
+then
+    # endpoint for system administrator
+    operation=cloudapi/1.0.0/sessions/provider
+    if [ "$org" != "System" -a "$org" != "system" ]
+    then
+        # endpoint for org users
+        operation=cloudapi/1.0.0/sessions
+    fi
+fi
+
+set -x
+curl -I -k --header "Accept: application/*;version=${api_version}.0" \
     --header "Authorization: Basic $auth" \
-    --request POST https://$IP/api/sessions
+    --request POST https://$IP/$operation
 
 # If successful, the output of this command will include lines like the following
 # X-VCLOUD-AUTHORIZATION: 08a321735de84f1d9ec80c3b3e18fa8b
@@ -25,18 +42,5 @@ curl -I -k --header "Accept: application/*;version=32.0" \
 #
 # The string after `X-VCLOUD-AUTHORIZATION:` is the old (deprecated) token.
 # The 612-character string after `X-VMWARE-VCLOUD-ACCESS-TOKEN` is the bearer token
-
-# For VCD version 10.0+, you can use one of the following commands instead.
-
-# PROVIDER:
-# curl -I -k --header "Accept: application/*;version=33.0" \
-#    --header "Authorization: Basic $auth" \
-#    --request POST https://$IP/cloudapi/1.0.0/sessions/provider
-
-# TENANT
-# curl -I -k --header "Accept: application/*;version=33.0" \
-#    --header "Authorization: Basic $auth" \
-#    --request POST https://$IP/cloudapi/1.0.0/sessions
 #
-# The cloudapi requests will only return the bearer token
-
+# Note that using cloudapi we will only get the bearer token
