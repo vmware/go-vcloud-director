f7f3ed6e570b90ec6e29da89c486e3c3832b2faa
Giuseppe Maxia <dataclouder@users.noreply.github.com>
2020-11-25 14:04:45 +0100

    Add list functions for edge gateway NSXV entities (#342)
    * Add functions 'edge.GetLbAppRules', 'edge.GetLbServerPools', 'edge.GetLbAppProfiles', 'edge.GetNsxvNatRules'
    * Add function client.GetOrgList
    * Export private function `client.maxSupportedVersion` to `client.MaxSupportedVersion`

--- govcd/lbapprule_test.go
+++ govcd/lbapprule_test.go
@@ -57,6 +57,18 @@ func (vcd *TestVCD) Test_LBAppRule(check *C) {
 
 	check.Assert(createdLbAppRule.Script, Equals, lbAppRuleConfig.Script)
 
+	// Test that we can extract a list of LB app rules, and that one of them is the rule we have got when searching by name
+	lbAppRules, err := edge.GetLbAppRules()
+	check.Assert(err, IsNil)
+	check.Assert(lbAppRules, NotNil)
+	foundRule := false
+	for _, rule := range lbAppRules {
+		if rule.Name == lbAppRuleByName.Name && rule.ID == lbAppRuleByName.ID {
+			foundRule = true
+		}
+	}
+	check.Assert(foundRule, Equals, true)
+
 	// Test updating fields
 	// Update script to be multi-line
 	lbAppRuleByID.Script = "acl other_page url_beg / other redirect location https://www.other.com/ ifother_page\n" +
