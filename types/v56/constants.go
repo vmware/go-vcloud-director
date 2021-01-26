53c8137d7369fb6d502ccc7cd885c7c77113e8c9
Giuseppe Maxia <gmaxia@vmware.com>
2021-01-26 09:24:43 +0100

    Add types to support standalone VM

--- types/v56/constants.go
+++ types/v56/constants.go
@@ -127,6 +127,10 @@ const (
 	MimeVdcComputePolicyReferences = "application/vnd.vmware.vcloud.vdcComputePolicyReferences+xml"
 	// Mime for Storage profile
 	MimeStorageProfile = "application/vnd.vmware.admin.vdcStorageProfile+xml "
+	// Mime for create VM Params
+	MimeCreateVmParams = "application/vnd.vmware.vcloud.CreateVmParams+xml"
+	// Mime for instantiate VM Params from template
+	MimeInstantiateVmTemplateParams = "application/vnd.vmware.vcloud.instantiateVmTemplateParams+xml"
 )
 
 const (
--- types/v56/vm_types.go
+++ types/v56/vm_types.go
@@ -59,6 +59,40 @@ type VM struct {
 	Media          *Reference      `xml:"Media,omitempty"`         // Reference to the media object to insert in a new VM.
 }
 
+type InnerVM struct {
+	//XMLName xml.Name `xml:"Vm"`
+	Ovf   string `xml:"xmlns:ovf,attr,omitempty"`
+	Xsi   string `xml:"xmlns:xsi,attr,omitempty"`
+	Xmlns string `xml:"xmlns,attr,omitempty"`
+
+	HREF                      string                     `xml:"href,attr,omitempty"`                    // The URI of the entity.
+	Type                      string                     `xml:"type,attr,omitempty"`                    // The MIME type of the entity.
+	ID                        string                     `xml:"id,attr,omitempty"`                      // The entity identifier, expressed in URN format. The value of this attribute uniquely identifies the entity, persists for the life of the entity, and is never reused
+	OperationKey              string                     `xml:"operationKey,attr,omitempty"`            // Optional unique identifier to support idempotent semantics for create and delete operations.
+	Name                      string                     `xml:"name,attr"`                              // The name of the entity.
+	Status                    int                        `xml:"status,attr,omitempty"`                  // Creation status of the resource entity.
+	Deployed                  bool                       `xml:"deployed,attr,omitempty"`                // True if the virtual machine is deployed.
+	NeedsCustomization        bool                       `xml:"needsCustomization,attr,omitempty"`      // True if this virtual machine needs customization.
+	NestedHypervisorEnabled   bool                       `xml:"nestedHypervisorEnabled,attr,omitempty"` // True if hardware-assisted CPU virtualization capabilities in the host should be exposed to the guest operating system.
+	Link                      LinkList                   `xml:"Link,omitempty"`                         // A reference to an entity or operation associated with this object.
+	Description               string                     `xml:"Description,omitempty"`                  // Optional description.
+	Tasks                     *TasksInProgress           `xml:"Tasks,omitempty"`                        // A list of queued, running, or recently completed tasks associated with this entity.
+	Files                     *FilesList                 `xml:"FilesList,omitempty"`                    // Represents a list of files to be transferred (uploaded or downloaded). Each File in the list is part of the ResourceEntity.
+	VAppParent                *Reference                 `xml:"VAppParent,omitempty"`                   // Reserved. Unimplemented.
+	DateCreated               string                     `xml:"DateCreated,omitempty"`                  // Creation date/time of the vApp.
+	VirtualHardwareSection    *VirtualHardwareSection    `xml:"VirtualHardwareSection,omitempty"`
+	NetworkConnectionSection  *NetworkConnectionSection  `xml:"NetworkConnectionSection,omitempty"`
+	VAppScopedLocalID         string                     `xml:"VAppScopedLocalId,omitempty"` // A unique identifier for the virtual machine in the scope of the vApp.
+	Snapshots                 *SnapshotSection           `xml:"SnapshotSection,omitempty"`
+	VmSpecSection             *VmSpecSection             `xml:"VmSpecSection,omitempty"`
+	GuestCustomizationSection *GuestCustomizationSection `xml:"GuestCustomizationSection,omitempty"`
+	VMCapabilities            *VmCapabilities            `xml:"VmCapabilities,omitempty"` // Allows you to specify certain capabilities of this virtual machine.
+	StorageProfile            *Reference                 `xml:"StorageProfile,omitempty"` // A reference to a storage profile to be used for this object. The specified storage profile must exist in the organization vDC that contains the object. If not specified, the default storage profile for the vDC is used.
+	ProductSection            *ProductSection            `xml:"ProductSection,omitempty"`
+	ComputePolicy             *ComputePolicy             `xml:"ComputePolicy,omitempty"` // accessible only from version API 33.0
+	Media                     *Reference                 `xml:"Media,omitempty"`         // Reference to the media object to insert in a new VM.
+}
+
 // VmSpecSection from VM struct
 type VmSpecSection struct {
 	Modified          *bool             `xml:"Modified,attr,omitempty"`
@@ -106,4 +140,36 @@ type ComputePolicy struct {
 	VmPlacementPolicyFinal *bool      `xml:"VmPlacementPolicyFinal,omitempty"` // True indicates that the placement policy cannot be removed from a VM that is instantiated with it. This value defaults to false.
 	VmSizingPolicy         *Reference `xml:"VmSizingPolicy,omitempty"`         // VdcComputePolicy that defines VM's sizing and resource allocation.
 	VmSizingPolicyFinal    *bool      `xml:"VmSizingPolicyFinal,omitempty"`    // True indicates that the sizing policy cannot be removed from a VM that is instantiated with it. This value defaults to false.
+}
+
+type CreateVmParams struct {
+	XMLName     xml.Name   `xml:"CreateVmParams"`
+	XmlnsOvf    string     `xml:"xmlns:ovf,attr"`
+	Xmlns       string     `xml:"xmlns,attr,omitempty"`
+	Name        string     `xml:"name,attr,omitempty"`    // Typically used to name or identify the subject of the request. For example, the name of the object being created or modified.
+	PowerOn     bool       `xml:"powerOn,attr,omitempty"` // True if the VM should be powered-on after creation. Defaults to false.
+	CreateVm    *InnerVM   `xml:"CreateVm"`               // Read-only information about the Vm to create. This information appears in the Task returned by a createVm request.
+	Media       *Reference `xml:"Media,omitempty"`        // Reference to the media object to insert in the new VM.
+	Description string     `xml:"Description,omitempty"`  // Optional description
+}
+
+type InstantiateVmTemplateParams struct {
+	XMLName               xml.Name                 `xml:"InstantiateVmTemplateParams"`
+	XmlnsOvf              string                   `xml:"xmlns:ovf,attr"`
+	Xmlns                 string                   `xml:"xmlns,attr,omitempty"`
+	Name                  string                   `xml:"name,attr,omitempty"`             // Typically used to name or identify the subject of the request. For example, the name of the object being created or modified.
+	PowerOn               bool                     `xml:"powerOn,attr,omitempty"`          // True if the VM should be powered-on after creation. Defaults to false.
+	Description           string                   `xml:"Description,omitempty"`           // Optional description
+	SourcedVmTemplateItem *SourcedVmTemplateParams `xml:"SourcedVmTemplateItem,omitempty"` // Represents virtual machine instantiation parameters.
+	AllEULAsAccepted      bool                     `xml:"AllEULAsAccepted,omitempty"`      // True confirms acceptance of all EULAs in a vApp template. Instantiation fails if this element is missing, empty, or set to false and one or more EulaSection elements are present.
+	ComputePolicy         *ComputePolicy           `xml:"ComputePolicy,omitempty"`         // A reference to a vdc compute policy. This contains VM's actual vdc compute policy reference and also optionally an add-on policy which always defines VM's sizing.
+}
+
+type SourcedVmTemplateParams struct {
+	LocalityParams                *LocalityParams      `xml:"LocalityParams,omitempty"`                // Locality parameters provide a hint that may help optimize placement of a VM and an independent a Disk so that the VM can make efficient use of the disk.
+	Source                        *Reference           `xml:"Source"`                                  // A reference to an existing VM template
+	StorageProfile                *Reference           `xml:"StorageProfile,omitempty"`                // A reference to a storage profile to be used for the Vm. The specified storage profile must exist in the organization vDC that contains the composed vApp. If not specified, the default storage profile for the vDC is used.
+	VmCapabilities                *VmCapabilities      `xml:"VmCapabilities,omitempty"`                // Describes the capabilities (hot swap, etc.) the instantiated VM should have.
+	VmGeneralParams               *VMGeneralParams     `xml:"VMGeneralParams,omitempty"`               // Specify name, description, and other properties of a VM during instantiation.
+	VmTemplateInstantiationParams *InstantiationParams `xml:"VmTemplateInstantiationParams,omitempty"` // Same as InstantiationParams used for VMs within a vApp
 }
