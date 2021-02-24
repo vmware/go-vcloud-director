6f23d66bf57fbd518d3c5f00e5ebcd02f2357a40
Dainius S <dserplis@vmware.com>
2021-02-24 22:22:10 +0200

    Redirect test output to /dev/null in tagverify

--- scripts/test-tags.sh
+++ scripts/test-tags.sh
@@ -23,7 +23,7 @@ echo "=== RUN TagsTest"
 for tag in $tags
 do
 
-    go test -tags $tag -timeout 0 -count=0 -check.vv
+    go test -tags $tag -timeout 0 -count=0 -check.vv > /dev/null
 
     if [ "$?" == "0" ]
     then
