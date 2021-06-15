35cbcf751a946d74f0dc85815f4a5a2a4aa32168
Giuseppe Maxia <gmaxia@vmware.com>
2021-06-15 13:43:03 +0200

    Fix right retrieval by name
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- govcd/rights.go
+++ govcd/rights.go
@@ -6,6 +6,7 @@ package govcd
 import (
 	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
 )
@@ -33,13 +34,13 @@ func getAllRights(client *Client, queryParameters url.Values, additionalHeader m
 	return typeResponses, nil
 }
 
-// GetAllRights retrieves all rights. Query parameters can be supplied to perform additional
-// filtering
+// GetAllRights retrieves all available rights.
+// Query parameters can be supplied to perform additional filtering
 func (client *Client) GetAllRights(queryParameters url.Values) ([]*types.Right, error) {
 	return getAllRights(client, queryParameters, nil)
 }
 
-// GetAllRights retrieves all rights using OpenAPI endpoint. Query parameters can be supplied to perform additional
+// GetAllRights retrieves all available rights. Query parameters can be supplied to perform additional
 // filtering
 func (adminOrg *AdminOrg) GetAllRights(queryParameters url.Values) ([]*types.Right, error) {
 	tenantContext, err := adminOrg.getTenantContext()
@@ -49,9 +50,9 @@ func (adminOrg *AdminOrg) GetAllRights(queryParameters url.Values) ([]*types.Rig
 	return getAllRights(adminOrg.client, queryParameters, getTenantContextHeader(tenantContext))
 }
 
-// getRoleRights retrieves all rights belonging to a given Role or similar container (global role, rights bundle).
+// getRights retrieves rights belonging to a given Role or similar container (global role, rights bundle).
 // Query parameters can be supplied to perform additional filtering
-func getRoleRights(client *Client, roleId, endpoint string, queryParameters url.Values, additionalHeader map[string]string) ([]*types.Right, error) {
+func getRights(client *Client, roleId, endpoint string, queryParameters url.Values, additionalHeader map[string]string) ([]*types.Right, error) {
 	minimumApiVersion, err := client.checkOpenApiEndpointCompatibility(endpoint)
 	if err != nil {
 		return nil, err
@@ -71,17 +72,29 @@ func getRoleRights(client *Client, roleId, endpoint string, queryParameters url.
 	return typeResponses, nil
 }
 
-// GetRoleRights retrieves all rights belonging to a given Role. Query parameters can be supplied to perform additional
+// GetRights retrieves all rights belonging to a given Role. Query parameters can be supplied to perform additional
 // filtering
-func (role *Role) GetRoleRights(queryParameters url.Values) ([]*types.Right, error) {
+func (role *Role) GetRights(queryParameters url.Values) ([]*types.Right, error) {
 	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointRoles
-	return getRoleRights(role.client, role.Role.ID, endpoint, queryParameters, getTenantContextHeader(role.TenantContext))
+	return getRights(role.client, role.Role.ID, endpoint, queryParameters, getTenantContextHeader(role.TenantContext))
 }
 
-// getRightByName retrieves rights by given name
+// getRightByName retrieves a right by given name
 func getRightByName(client *Client, name string, additionalHeader map[string]string) (*types.Right, error) {
 	var params = url.Values{}
-	params.Set("filter", "name=="+name)
+
+	slowSearch := false
+
+	// When the right name contains commas or semicolons, the encoding is rejected by the API.
+	// For this reason, when one or more commas or semicolons are present (6 occurrences in more than 300 right names)
+	// we run the search brute force, by fetching all the rights, and comparing the names.
+	// This problem should be fixed in 10.3
+	// TODO: revisit this function after 10.3 is released
+	if strings.Contains(name, ",") || strings.Contains(name, ";") {
+		slowSearch = true
+	} else {
+		params.Set("filter", "name=="+name)
+	}
 	rights, err := getAllRights(client, params, additionalHeader)
 	if err != nil {
 		return nil, err
@@ -89,18 +102,28 @@ func getRightByName(client *Client, name string, additionalHeader map[string]str
 	if len(rights) == 0 {
 		return nil, ErrorEntityNotFound
 	}
+
+	if slowSearch {
+		for _, right := range rights {
+			if right.Name == name {
+				return right, nil
+			}
+		}
+		return nil, ErrorEntityNotFound
+	}
+
 	if len(rights) > 1 {
 		return nil, fmt.Errorf("more than one right found with name '%s'", name)
 	}
 	return rights[0], nil
 }
 
-// GetRightByName retrieves rights by given name
+// GetRightByName retrieves right by given name
 func (client *Client) GetRightByName(name string) (*types.Right, error) {
 	return getRightByName(client, name, nil)
 }
 
-// GetRightByName retrieves rights by given name
+// GetRightByName retrieves right by given name
 func (adminOrg *AdminOrg) GetRightByName(name string) (*types.Right, error) {
 	tenantContext, err := adminOrg.getTenantContext()
 	if err != nil {
