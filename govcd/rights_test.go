1d5ef4b0440b76833eb5d54fcb3ca1128135f62f
Giuseppe Maxia <gmaxia@vmware.com>
2021-06-15 13:43:56 +0200

    Improve output of rights tests
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- govcd/rights_test.go
+++ govcd/rights_test.go
@@ -22,15 +22,17 @@ func (vcd *TestVCD) Test_TenantContext(check *C) {
 	check.Assert(clientRoles, NotNil)
 	check.Assert(len(clientRoles), Not(Equals), 0)
 
-	fmt.Println("Client roles")
-	for _, role := range clientRoles {
-		fmt.Printf("%-40s %s\n", role.Role.Name, role.Role.Description)
-		rights, err := role.GetRoleRights(nil)
-		check.Assert(err, IsNil)
-		for i, right := range rights {
-			fmt.Printf("\t%3d %s\n", i+1, right.Name)
+	if testVerbose {
+		fmt.Println("Client roles")
+		for _, role := range clientRoles {
+			fmt.Printf("%-40s %s\n", role.Role.Name, role.Role.Description)
+			rights, err := role.GetRights(nil)
+			check.Assert(err, IsNil)
+			for i, right := range rights {
+				fmt.Printf("\t%3d %s\n", i+1, right.Name)
+			}
+			fmt.Println()
 		}
-		fmt.Println()
 	}
 
 	orgRoles, err := adminOrg.GetAllRoles(nil)
@@ -38,15 +40,17 @@ func (vcd *TestVCD) Test_TenantContext(check *C) {
 	check.Assert(orgRoles, NotNil)
 	check.Assert(len(orgRoles), Not(Equals), 0)
 
-	fmt.Println("ORG roles")
-	for _, role := range orgRoles {
-		fmt.Printf("%-40s %s\n", role.Role.Name, role.Role.Description)
-		rights, err := role.GetRoleRights(nil)
-		check.Assert(err, IsNil)
-		for i, right := range rights {
-			fmt.Printf("\t%3d %s\n", i+1, right.Name)
+	if testVerbose {
+		fmt.Println("ORG roles")
+		for _, role := range orgRoles {
+			fmt.Printf("%-40s %s\n", role.Role.Name, role.Role.Description)
+			rights, err := role.GetRights(nil)
+			check.Assert(err, IsNil)
+			for i, right := range rights {
+				fmt.Printf("\t%3d %s\n", i+1, right.Name)
+			}
+			fmt.Println()
 		}
-		fmt.Println()
 	}
 
 }
@@ -56,23 +60,55 @@ func (vcd *TestVCD) Test_Rights(check *C) {
 	check.Assert(err, IsNil)
 	check.Assert(adminOrg, NotNil)
 
-	allExistingRights, err := adminOrg.GetAllRights(nil)
+	allOrgRights, err := adminOrg.GetAllRights(nil)
+	check.Assert(err, IsNil)
+	check.Assert(allOrgRights, NotNil)
+
+	if testVerbose {
+		fmt.Printf("(org) how many rights: %d\n", len(allOrgRights))
+		for i, oneRight := range allOrgRights {
+			fmt.Printf("%3d %-20s %-53s %s\n", i, oneRight.Name, oneRight.ID, oneRight.Category)
+		}
+	}
+	allExistingRights, err := adminOrg.client.GetAllRights(nil)
 	check.Assert(err, IsNil)
 	check.Assert(allExistingRights, NotNil)
 
-	fmt.Printf("(org) how many rights: %d\n", len(allExistingRights))
-
-	for i, oneRight := range allExistingRights {
-		fmt.Printf("%3d %-20s %-53s %s\n", i, oneRight.Name, oneRight.ID, oneRight.Category)
+	if testVerbose {
+		fmt.Printf("(global) how many rights: %d\n", len(allExistingRights))
+		for i, oneRight := range allExistingRights {
+			fmt.Printf("%3d %-20s %-53s %s\n", i, oneRight.Name, oneRight.ID, oneRight.Category)
+		}
 	}
-	allExistingRights, err = adminOrg.client.GetAllRights(nil)
+	// Test a sample of rights
+	maxItems := 10
+
+	for i, right := range allExistingRights {
+		searchRight(adminOrg.client, right.Name, right.ID, check)
+		if i > maxItems {
+			break
+		}
+	}
+	var rigthNamesWithCommas = []string{
+		"vApp: VM Migrate, Force Undeploy, Relocate, Consolidate",
+		"Role: Create, Edit, Delete, or Copy",
+		"Task: Resume, Abort, or Fail",
+		"VCD Extension: Register, Unregister, Refresh, Associate or Disassociate",
+		"UI Plugins: Define, Upload, Modify, Delete, Associate or Disassociate",
+	}
+	for _, name := range rigthNamesWithCommas {
+		searchRight(adminOrg.client, name, "", check)
+	}
+
+}
+
+func searchRight(client *Client, name, id string, check *C) {
+	fullRightByName, err := client.GetRightByName(name)
 	check.Assert(err, IsNil)
-	check.Assert(allExistingRights, NotNil)
-
-	fmt.Printf("(global) how many rights: %d\n", len(allExistingRights))
-
-	for i, oneRight := range allExistingRights {
-		fmt.Printf("%3d %-20s %-53s %s\n", i, oneRight.Name, oneRight.ID, oneRight.Category)
+	check.Assert(fullRightByName, NotNil)
+	if id != "" {
+		fullRightById, err := client.GetRightById(id)
+		check.Assert(err, IsNil)
+		check.Assert(fullRightById, NotNil)
 	}
-
 }
