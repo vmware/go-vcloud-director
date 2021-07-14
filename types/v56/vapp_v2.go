8e29ea4cec0f1b7f05969878875df79d638784ac
Giuseppe Maxia <gmaxia@vmware.com>
2021-07-14 16:33:10 +0200

    Add type MoveVmParams
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- types/v56/vapp_v2.go
+++ types/v56/vapp_v2.go
@@ -4,34 +4,6 @@ import "encoding/xml"
 
 type VAppV2 VApp
 
-type SourcedCompositionItemParamV2 struct {
-	// Attributes
-	SourceDelete bool `xml:"sourceDelete,attr,omitempty"` // True if the source item should be deleted after composition is complete.
-	// Elements
-	Source              *Reference           `xml:"Source"`                        // Reference to a vApp, vApp template or virtual machine to include in the composition. Changing the name of the newly created VM by specifying name attribute is deprecated. Include VmGeneralParams element instead.
-	VMGeneralParams     *VMGeneralParams     `xml:"VmGeneralParams,omitempty"`     // Specify name, description, and other properties of a VM during instantiation.
-	VAppScopedLocalID   string               `xml:"VAppScopedLocalId,omitempty"`   // If Source references a VM, this value provides a unique identifier for the VM in the scope of the composed vApp.
-	InstantiationParams *InstantiationParams `xml:"InstantiationParams,omitempty"` // If Source references a VM this can include any of the following OVF sections: VirtualHardwareSection OperatingSystemSection NetworkConnectionSection GuestCustomizationSection.
-	NetworkAssignment   []*NetworkAssignment `xml:"NetworkAssignment,omitempty"`   // If Source references a VM, this element maps a network name specified in the VM to the network name of a vApp network defined in the composed vApp.
-	StorageProfile      *Reference           `xml:"StorageProfile,omitempty"`      // If Source references a VM, this element contains a reference to a storage profile to be used for the VM. The specified storage profile must exist in the organization vDC that contains the composed vApp. If not specified, the default storage profile for the vDC is used.
-	LocalityParams      *LocalityParams      `xml:"LocalityParams,omitempty"`      // Represents locality parameters. Locality parameters provide a hint that may help the placement engine optimize placement of a VM and an independent a Disk so that the VM can make efficient use of the disk.
-	ComputePolicy       *ComputePolicy       `xml:"ComputePolicy,omitempty"`       // accessible only from version API 33.0
-}
-
-type CreatedCompositionItemParamV2 struct {
-	// Attributes
-	SourceDelete bool `xml:"sourceDelete,attr,omitempty"` // True if the source item should be deleted after composition is complete.
-	// Elements
-	CreateItem          *CreateItem          `xml:"CreateItem,omitempty"`
-	VMGeneralParams     *VMGeneralParams     `xml:"VmGeneralParams,omitempty"`     // Specify name, description, and other properties of a VM during instantiation.
-	VAppScopedLocalID   string               `xml:"VAppScopedLocalId,omitempty"`   // If Source references a VM, this value provides a unique identifier for the VM in the scope of the composed vApp.
-	InstantiationParams *InstantiationParams `xml:"InstantiationParams,omitempty"` // If Source references a VM this can include any of the following OVF sections: VirtualHardwareSection OperatingSystemSection NetworkConnectionSection GuestCustomizationSection.
-	NetworkAssignment   []*NetworkAssignment `xml:"NetworkAssignment,omitempty"`   // If Source references a VM, this element maps a network name specified in the VM to the network name of a vApp network defined in the composed vApp.
-	StorageProfile      *Reference           `xml:"StorageProfile,omitempty"`      // If Source references a VM, this element contains a reference to a storage profile to be used for the VM. The specified storage profile must exist in the organization vDC that contains the composed vApp. If not specified, the default storage profile for the vDC is used.
-	LocalityParams      *LocalityParams      `xml:"LocalityParams,omitempty"`      // Represents locality parameters. Locality parameters provide a hint that may help the placement engine optimize placement of a VM and an independent a Disk so that the VM can make efficient use of the disk.
-	ComputePolicy       *ComputePolicy       `xml:"ComputePolicy,omitempty"`       // accessible only from version API 33.0
-}
-
 type ComposeVAppParamsV2 struct {
 	XMLName xml.Name `xml:"ComposeVAppParams"`
 	Ovf     string   `xml:"xmlns:ovf,attr"`
@@ -43,10 +15,10 @@ type ComposeVAppParamsV2 struct {
 	PowerOn     bool   `xml:"powerOn,attr"`               // True if the vApp should be powered-on at instantiation. Defaults to true.
 	LinkedClone bool   `xml:"linkedClone,attr,omitempty"` // Reserved. Unimplemented.
 	// Elements
-	Description         string                           `xml:"Description,omitempty"`         // Optional description.
-	VAppParent          *Reference                       `xml:"VAppParent,omitempty"`          // Reserved. Unimplemented.
-	InstantiationParams *InstantiationParams             `xml:"InstantiationParams,omitempty"` // Instantiation parameters for the composed vApp.
-	SourcedItem         []*SourcedCompositionItemParamV2 `xml:"SourcedItem,omitempty"`         // Composition items. One of: vApp vAppTemplate VM.
-	CreateItem         []*CreateItem `xml:"CreateItem,omitempty"`          // Composition items. One of: vApp vAppTemplate VM.
-	AllEULAsAccepted    bool                             `xml:"AllEULAsAccepted,omitempty"`    // True confirms acceptance of all EULAs in a vApp template. Instantiation fails if this element is missing, empty, or set to false and one or more EulaSection elements are present.
+	Description         string                         `xml:"Description,omitempty"`         // Optional description.
+	VAppParent          *Reference                     `xml:"VAppParent,omitempty"`          // Reserved. Unimplemented.
+	InstantiationParams *InstantiationParams           `xml:"InstantiationParams,omitempty"` // Instantiation parameters for the composed vApp.
+	SourcedItem         []*SourcedCompositionItemParam `xml:"SourcedItem,omitempty"`         // Composition items. One of: vApp vAppTemplate VM.
+	CreateItem          []*CreateItem                  `xml:"CreateItem,omitempty"`          // Composition items. One of: vApp vAppTemplate VM.
+	AllEULAsAccepted    bool                           `xml:"AllEULAsAccepted,omitempty"`    // True confirms acceptance of all EULAs in a vApp template. Instantiation fails if this element is missing, empty, or set to false and one or more EulaSection elements are present.
 }
