vmware/go-vcloud-director

commit 79873f448937c5e29bf612abb9115d05c94ad3b9

Fix receiver name in client and comments (#379)
* Fix receiver name in code and comments

Co-authored-by: Yuri crashiura 
Co-authored-by: Dainius <Didainius@users.noreply.github.com>
Co-authored-by: lvirbalas <lvirbalas@vmware.com>
Co-authored-by: Giuseppe Maxia <dataclouder@users.noreply.github.com>

govcd/api.go
@@ -137,7 +137,7 @@ func debugShowResponse(resp *http.Response, body []byte) {
 	}
 }
 
-// Convenience function, similar to os.IsNotExist that checks whether a given error
+// IsNotFound is a convenience function, similar to os.IsNotExist that checks whether a given error
 // is a "Not found" error, such as
 // if isNotFound(err) {
 //    // do what is needed in case of not found
@@ -154,8 +154,8 @@ func ContainsNotFound(err error) bool {
 }
 
 // NewRequestWitNotEncodedParams allows passing complex values params that shouldn't be encoded like for queries. e.g. /query?filter=name=foo
-func (cli *Client) NewRequestWitNotEncodedParams(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader) *http.Request {
-	return cli.NewRequestWitNotEncodedParamsWithApiVersion(params, notEncodedParams, method, reqUrl, body, cli.APIVersion)
+func (client *Client) NewRequestWitNotEncodedParams(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader) *http.Request {
+	return client.NewRequestWitNotEncodedParamsWithApiVersion(params, notEncodedParams, method, reqUrl, body, client.APIVersion)
 }
 
 // NewRequestWitNotEncodedParamsWithApiVersion allows passing complex values params that shouldn't be encoded like for queries. e.g. /query?filter=name=foo
@@ -165,13 +165,13 @@ func (cli *Client) NewRequestWitNotEncodedParams(params map[string]string, notEn
 // * reqUrl - request url
 // * body - request body
 // * apiVersion - provided Api version overrides default Api version value used in request parameter
-func (cli *Client) NewRequestWitNotEncodedParamsWithApiVersion(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string) *http.Request {
-	return cli.newRequest(params, notEncodedParams, method, reqUrl, body, apiVersion, nil)
+func (client *Client) NewRequestWitNotEncodedParamsWithApiVersion(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string) *http.Request {
+	return client.newRequest(params, notEncodedParams, method, reqUrl, body, apiVersion, nil)
 }
 
 // newRequest is the parent of many "specific" "NewRequest" functions.
 // Note. It is kept private to avoid breaking public API on every new field addition.
-func (cli *Client) newRequest(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string, additionalHeader http.Header) *http.Request {
+func (client *Client) newRequest(params map[string]string, notEncodedParams map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string, additionalHeader http.Header) *http.Request {
 	reqValues := url.Values{}
 
 	// Build up our request parameters
@@ -201,20 +201,20 @@ func (cli *Client) newRequest(params map[string]string, notEncodedParams map[str
 	// error only if can't process an url.ParseRequestURI().
 	req, _ := http.NewRequest(method, reqUrl.String(), body)
 
-	if cli.VCDAuthHeader != "" && cli.VCDToken != "" {
+	if client.VCDAuthHeader != "" && client.VCDToken != "" {
 		// Add the authorization header
-		req.Header.Add(cli.VCDAuthHeader, cli.VCDToken)
+		req.Header.Add(client.VCDAuthHeader, client.VCDToken)
 	}
-	if (cli.VCDAuthHeader != "" && cli.VCDToken != "") ||
+	if (client.VCDAuthHeader != "" && client.VCDToken != "") ||
 		(additionalHeader != nil && additionalHeader.Get("Authorization") != "") {
 		// Add the Accept header for VCD
 		req.Header.Add("Accept", "application/*+xml;version="+apiVersion)
 	}
 	// The deprecated authorization token is 32 characters long
 	// The bearer token is 612 characters long
-	if len(cli.VCDToken) > 32 {
+	if len(client.VCDToken) > 32 {
 		req.Header.Add("X-Vmware-Vcloud-Token-Type", "Bearer")
-		req.Header.Add("Authorization", "bearer "+cli.VCDToken)
+		req.Header.Add("Authorization", "bearer "+client.VCDToken)
 	}
 
 	// Merge in additional headers before logging if any where specified in additionalHeader
@@ -234,7 +234,7 @@ func (cli *Client) newRequest(params map[string]string, notEncodedParams map[str
 		}
 	}
 
-	setHttpUserAgent(cli.UserAgent, req)
+	setHttpUserAgent(client.UserAgent, req)
 
 	// Avoids passing data if the logging of requests is disabled
 	if util.LogHttpRequest {
@@ -251,14 +251,14 @@ func (cli *Client) newRequest(params map[string]string, notEncodedParams map[str
 }
 
 // NewRequest creates a new HTTP request and applies necessary auth headers if set.
-func (cli *Client) NewRequest(params map[string]string, method string, reqUrl url.URL, body io.Reader) *http.Request {
-	return cli.NewRequestWitNotEncodedParams(params, nil, method, reqUrl, body)
+func (client *Client) NewRequest(params map[string]string, method string, reqUrl url.URL, body io.Reader) *http.Request {
+	return client.NewRequestWitNotEncodedParams(params, nil, method, reqUrl, body)
 }
 
 // NewRequestWithApiVersion creates a new HTTP request and applies necessary auth headers if set.
 // Allows to override default request API Version
-func (cli *Client) NewRequestWithApiVersion(params map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string) *http.Request {
-	return cli.NewRequestWitNotEncodedParamsWithApiVersion(params, nil, method, reqUrl, body, apiVersion)
+func (client *Client) NewRequestWithApiVersion(params map[string]string, method string, reqUrl url.URL, body io.Reader, apiVersion string) *http.Request {
+	return client.NewRequestWitNotEncodedParamsWithApiVersion(params, nil, method, reqUrl, body, apiVersion)
 }
 
 // ParseErr takes an error XML resp, error interface for unmarshalling and returns a single string for
@@ -387,7 +387,7 @@ func checkRespWithErrType(bodyType types.BodyType, resp *http.Response, err, err
 	}
 }
 
-// Helper function creates request, runs it, checks response and parses task from response.
+// ExecuteTaskRequest helper function creates request, runs it, checks response and parses task from response.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -398,7 +398,7 @@ func (client *Client) ExecuteTaskRequest(pathURL, requestType, contentType, erro
 	return client.executeTaskRequest(pathURL, requestType, contentType, errorMessage, payload, client.APIVersion)
 }
 
-// Helper function creates request, runs it, checks response and parses task from response.
+// ExecuteTaskRequestWithApiVersion helper function creates request, runs it, checks response and parses task from response.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -444,7 +444,7 @@ func (client *Client) executeTaskRequest(pathURL, requestType, contentType, erro
 	return *task, nil
 }
 
-// Helper function creates request, runs it, checks response and do not expect any values from it.
+// ExecuteRequestWithoutResponse helper function creates request, runs it, checks response and do not expect any values from it.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -455,7 +455,7 @@ func (client *Client) ExecuteRequestWithoutResponse(pathURL, requestType, conten
 	return client.executeRequestWithoutResponse(pathURL, requestType, contentType, errorMessage, payload, client.APIVersion)
 }
 
-// Helper function creates request, runs it, checks response and do not expect any values from it.
+// ExecuteRequestWithoutResponseWithApiVersion helper function creates request, runs it, checks response and do not expect any values from it.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -499,7 +499,7 @@ func (client *Client) executeRequestWithoutResponse(pathURL, requestType, conten
 	return nil
 }
 
-// Helper function creates request, runs it, check responses and parses out interface from response.
+// ExecuteRequest helper function creates request, runs it, check responses and parses out interface from response.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -512,7 +512,7 @@ func (client *Client) ExecuteRequest(pathURL, requestType, contentType, errorMes
 	return client.executeRequest(pathURL, requestType, contentType, errorMessage, payload, out, client.APIVersion)
 }
 
-// Helper function creates request, runs it, check responses and parses out interface from response.
+// ExecuteRequestWithApiVersion helper function creates request, runs it, check responses and parses out interface from response.
 // pathURL - request URL
 // requestType - HTTP method type
 // contentType - value to set for "Content-Type"
@@ -606,7 +606,10 @@ func executeRequestWithApiVersion(pathURL, requestType, contentType string, payl
 
 // executeRequestCustomErr performs request and unmarshals API error to errType if not 2xx status was returned
 func executeRequestCustomErr(pathURL string, params map[string]string, requestType, contentType string, payload interface{}, client *Client, errType error, apiVersion string) (*http.Response, error) {
-	url, _ := url.ParseRequestURI(pathURL)
+	requestURI, err := url.ParseRequestURI(pathURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse path request URI '%s': %s", pathURL, err)
+	}
 
 	var req *http.Request
 	switch {
@@ -619,10 +622,10 @@ func executeRequestCustomErr(pathURL string, params map[string]string, requestTy
 		}
 		body := bytes.NewBufferString(xml.Header + string(marshaledXml))
 
-		req = client.NewRequestWithApiVersion(params, requestType, *url, body, apiVersion)
+		req = client.NewRequestWithApiVersion(params, requestType, *requestURI, body, apiVersion)
 
 	default:
-		req = client.NewRequestWithApiVersion(params, requestType, *url, nil, apiVersion)
+		req = client.NewRequestWithApiVersion(params, requestType, *requestURI, nil, apiVersion)
 	}
 
 	if contentType != "" {

govcd/admincatalog.go
@@ -37,7 +37,7 @@ func (adminCatalog *AdminCatalog) Delete(force, recursive bool) error {
 	return catalog.Delete(force, recursive)
 }
 
-// Updates the Catalog definition from current Catalog struct contents.
+// Update updates the Catalog definition from current Catalog struct contents.
 // Any differences that may be legally applied will be updated.
 // Returns an error if the call to vCD fails. Update automatically performs
 // a refresh with the admin catalog it gets back from the rest api
@@ -60,7 +60,7 @@ func (adminCatalog *AdminCatalog) Update() error {
 	return err
 }
 
-// Uploads an ova file to a catalog. This method only uploads bits to vCD spool area.
+// UploadOvf uploads an ova file to a catalog. This method only uploads bits to vCD spool area.
 // Returns errors if any occur during upload from vCD or upload process. On upload fail client may need to
 // remove vCD catalog item which waits for files to be uploaded. Files from ova are extracted to system
 // temp folder "govcd+random number" and left for inspection on error.

govcd/api_vcd_versions.go
@@ -74,21 +74,21 @@ var _ = vcdVersionToApiVersion
 // Format: ">= 27.0, < 32.0", ">= 30.0", "= 27.0"
 //
 // vCD version mapping to API version support https://code.vmware.com/doc/preview?id=8072
-func (cli *Client) APIVCDMaxVersionIs(versionConstraint string) bool {
-	err := cli.vcdFetchSupportedVersions()
+func (client *Client) APIVCDMaxVersionIs(versionConstraint string) bool {
+	err := client.vcdFetchSupportedVersions()
 	if err != nil {
 		util.Logger.Printf("[ERROR] could not retrieve supported versions: %s", err)
 		return false
 	}
 
 	util.Logger.Printf("[TRACE] checking max API version against constraints '%s'", versionConstraint)
-	maxVersion, err := cli.MaxSupportedVersion()
+	maxVersion, err := client.MaxSupportedVersion()
 	if err != nil {
 		util.Logger.Printf("[ERROR] unable to find max supported version : %s", err)
 		return false
 	}
 
-	isSupported, err := cli.apiVersionMatchesConstraint(maxVersion, versionConstraint)
+	isSupported, err := client.apiVersionMatchesConstraint(maxVersion, versionConstraint)
 	if err != nil {
 		util.Logger.Printf("[ERROR] unable to find max supported version : %s", err)
 		return false
@@ -104,11 +104,11 @@ func (cli *Client) APIVCDMaxVersionIs(versionConstraint string) bool {
 // Format: ">= 27.0, < 32.0", ">= 30.0", "= 27.0"
 //
 // vCD version mapping to API version support https://code.vmware.com/doc/preview?id=8072
-func (cli *Client) APIClientVersionIs(versionConstraint string) bool {
+func (client *Client) APIClientVersionIs(versionConstraint string) bool {
 
 	util.Logger.Printf("[TRACE] checking current API version against constraints '%s'", versionConstraint)
 
-	isSupported, err := cli.apiVersionMatchesConstraint(cli.APIVersion, versionConstraint)
+	isSupported, err := client.apiVersionMatchesConstraint(client.APIVersion, versionConstraint)
 	if err != nil {
 		util.Logger.Printf("[ERROR] unable to find cur supported version : %s", err)
 		return false
@@ -120,26 +120,26 @@ func (cli *Client) APIClientVersionIs(versionConstraint string) bool {
 // vcdFetchSupportedVersions retrieves list of supported versions from
 // /api/versions endpoint and stores them in VCDClient for future uses.
 // It only does it once.
-func (cli *Client) vcdFetchSupportedVersions() error {
+func (client *Client) vcdFetchSupportedVersions() error {
 	// Only fetch /versions if it is not stored already
-	numVersions := len(cli.supportedVersions.VersionInfos)
+	numVersions := len(client.supportedVersions.VersionInfos)
 	if numVersions > 0 {
 		util.Logger.Printf("[TRACE] skipping fetch of versions because %d are stored", numVersions)
 		return nil
 	}
 
-	apiEndpoint := cli.VCDHREF
+	apiEndpoint := client.VCDHREF
 	apiEndpoint.Path += "/versions"
 
 	suppVersions := new(SupportedVersions)
-	_, err := cli.ExecuteRequest(apiEndpoint.String(), http.MethodGet,
+	_, err := client.ExecuteRequest(apiEndpoint.String(), http.MethodGet,
 		"", "error fetching versions: %s", nil, suppVersions)
 
-	cli.supportedVersions = *suppVersions
+	client.supportedVersions = *suppVersions
 
 	// Log all supported API versions in one line to help identify vCD version from logs
-	allApiVersions := make([]string, len(cli.supportedVersions.VersionInfos))
-	for versionIndex, version := range cli.supportedVersions.VersionInfos {
+	allApiVersions := make([]string, len(client.supportedVersions.VersionInfos))
+	for versionIndex, version := range client.supportedVersions.VersionInfos {
 		allApiVersions[versionIndex] = version.Version
 	}
 	util.Logger.Printf("[DEBUG] supported API versions : %s", strings.Join(allApiVersions, ","))
@@ -148,9 +148,9 @@ func (cli *Client) vcdFetchSupportedVersions() error {
 }
 
 // MaxSupportedVersion parses supported version list and returns the highest version in string format.
-func (cli *Client) MaxSupportedVersion() (string, error) {
-	versions := make([]*semver.Version, len(cli.supportedVersions.VersionInfos))
-	for index, versionInfo := range cli.supportedVersions.VersionInfos {
+func (client *Client) MaxSupportedVersion() (string, error) {
+	versions := make([]*semver.Version, len(client.supportedVersions.VersionInfos))
+	for index, versionInfo := range client.supportedVersions.VersionInfos {
 		version, _ := semver.NewVersion(versionInfo.Version)
 		versions[index] = version
 	}
@@ -169,15 +169,15 @@ func (cli *Client) MaxSupportedVersion() (string, error) {
 
 // vcdCheckSupportedVersion checks if there is at least one specified version exactly matching listed ones.
 // Format example "27.0"
-func (cli *Client) vcdCheckSupportedVersion(version string) (bool, error) {
-	return cli.checkSupportedVersionConstraint(fmt.Sprintf("= %s", version))
+func (client *Client) vcdCheckSupportedVersion(version string) (bool, error) {
+	return client.checkSupportedVersionConstraint(fmt.Sprintf("= %s", version))
 }
 
 // Checks if there is at least one specified version matching the list returned by vCD.
 // Constraint format can be in format ">= 27.0, < 32",">= 30" ,"= 27.0".
-func (cli *Client) checkSupportedVersionConstraint(versionConstraint string) (bool, error) {
-	for _, versionInfo := range cli.supportedVersions.VersionInfos {
-		versionMatch, err := cli.apiVersionMatchesConstraint(versionInfo.Version, versionConstraint)
+func (client *Client) checkSupportedVersionConstraint(versionConstraint string) (bool, error) {
+	for _, versionInfo := range client.supportedVersions.VersionInfos {
+		versionMatch, err := client.apiVersionMatchesConstraint(versionInfo.Version, versionConstraint)
 		if err != nil {
 			return false, fmt.Errorf("cannot match version: %s", err)
 		}
@@ -189,7 +189,7 @@ func (cli *Client) checkSupportedVersionConstraint(versionConstraint string) (bo
 	return false, fmt.Errorf("version %s is not supported", versionConstraint)
 }
 
-func (cli *Client) apiVersionMatchesConstraint(version, versionConstraint string) (bool, error) {
+func (client *Client) apiVersionMatchesConstraint(version, versionConstraint string) (bool, error) {
 
 	checkVer, err := semver.NewVersion(version)
 	if err != nil {
@@ -210,15 +210,15 @@ func (cli *Client) apiVersionMatchesConstraint(version, versionConstraint string
 }
 
 // validateAPIVersion fetches API versions
-func (cli *Client) validateAPIVersion() error {
-	err := cli.vcdFetchSupportedVersions()
+func (client *Client) validateAPIVersion() error {
+	err := client.vcdFetchSupportedVersions()
 	if err != nil {
 		return fmt.Errorf("could not retrieve supported versions: %s", err)
 	}
 
 	// Check if version is supported
-	if ok, err := cli.vcdCheckSupportedVersion(cli.APIVersion); !ok || err != nil {
-		return fmt.Errorf("API version %s is not supported: %s", cli.APIVersion, err)
+	if ok, err := client.vcdCheckSupportedVersion(client.APIVersion); !ok || err != nil {
+		return fmt.Errorf("API version %s is not supported: %s", client.APIVersion, err)
 	}
 
 	return nil
@@ -227,21 +227,21 @@ func (cli *Client) validateAPIVersion() error {
 // GetSpecificApiVersionOnCondition returns default version or wantedApiVersion if it is connected to version
 // described in vcdApiVersionCondition
 // f.e. values ">= 32.0", "32.0" returns 32.0 if vCD version is above or 9.7
-func (cli *Client) GetSpecificApiVersionOnCondition(vcdApiVersionCondition, wantedApiVersion string) string {
-	apiVersion := cli.APIVersion
-	if cli.APIVCDMaxVersionIs(vcdApiVersionCondition) {
+func (client *Client) GetSpecificApiVersionOnCondition(vcdApiVersionCondition, wantedApiVersion string) string {
+	apiVersion := client.APIVersion
+	if client.APIVCDMaxVersionIs(vcdApiVersionCondition) {
 		apiVersion = wantedApiVersion
 	}
 	return apiVersion
 }
 
 // GetVcdVersion finds the VCD version and the time of build
-func (cli *Client) GetVcdVersion() (string, time.Time, error) {
+func (client *Client) GetVcdVersion() (string, time.Time, error) {
 
-	path := cli.VCDHREF
+	path := client.VCDHREF
 	path.Path += "/admin"
 	var admin types.VCloud
-	_, err := cli.ExecuteRequest(path.String(), http.MethodGet,
+	_, err := client.ExecuteRequest(path.String(), http.MethodGet,
 		"", "error retrieving admin info: %s", nil, &admin)
 	if err != nil {
 		return "", time.Time{}, err
@@ -269,9 +269,9 @@ func (cli *Client) GetVcdVersion() (string, time.Time, error) {
 }
 
 // GetVcdShortVersion returns the VCD version (three digits, no build info)
-func (cli *Client) GetVcdShortVersion() (string, error) {
+func (client *Client) GetVcdShortVersion() (string, error) {
 
-	vcdVersion, err := cli.GetVcdFullVersion()
+	vcdVersion, err := client.GetVcdFullVersion()
 	if err != nil {
 		return "", fmt.Errorf("error getting version digits: %s", err)
 	}
@@ -280,9 +280,9 @@ func (cli *Client) GetVcdShortVersion() (string, error) {
 }
 
 // GetVcdFullVersion returns the full VCD version information as a structure
-func (cli *Client) GetVcdFullVersion() (VcdVersion, error) {
+func (client *Client) GetVcdFullVersion() (VcdVersion, error) {
 	var vcdVersion VcdVersion
-	version, versionTime, err := cli.GetVcdVersion()
+	version, versionTime, err := client.GetVcdVersion()
 	if err != nil {
 		return VcdVersion{}, err
 	}
@@ -324,9 +324,9 @@ func intListToVersion(digits []int, atMost int) string {
 //  client version is 1.2.3.1234
 //  compare version is 1.1.1.0
 // function returns true regardless of value of howManyDigits
-func (cli *Client) VersionEqualOrGreater(compareTo string, howManyDigits int) (bool, error) {
+func (client *Client) VersionEqualOrGreater(compareTo string, howManyDigits int) (bool, error) {
 
-	fullVersion, err := cli.GetVcdFullVersion()
+	fullVersion, err := client.GetVcdFullVersion()
 	if err != nil {
 		return false, err
 	}

govcd/vm.go
@@ -34,7 +34,7 @@ func NewVM(cli *Client) *VM {
 	}
 }
 
-// create instance with reference to types.QueryResultVMRecordType
+// NewVMRecord creates an instance with reference to types.QueryResultVMRecordType
 func NewVMRecord(cli *Client) *VMRecord {
 	return &VMRecord{
 		VM:     new(types.QueryResultVMRecordType),
@@ -141,11 +141,11 @@ func (vm *VM) UpdateNetworkConnectionSection(networks *types.NetworkConnectionSe
 }
 
 // Deprecated: use client.GetVMByHref instead
-func (cli *Client) FindVMByHREF(vmHREF string) (VM, error) {
+func (client *Client) FindVMByHREF(vmHREF string) (VM, error) {
 
-	newVm := NewVM(cli)
+	newVm := NewVM(client)
 
-	_, err := cli.ExecuteRequest(vmHREF, http.MethodGet,
+	_, err := client.ExecuteRequest(vmHREF, http.MethodGet,
 		"", "error retrieving VM: %s", nil, newVm.VM)
 
 	return *newVm, err
@@ -213,7 +213,7 @@ func (vm *VM) PowerOff() (Task, error) {
 		"", "error powering off VM: %s", nil)
 }
 
-// Sets number of available virtual logical processors
+// ChangeCPUCount sets number of available virtual logical processors
 // (i.e. CPUs x cores per socket)
 // Cpu cores count is inherited from template.
 // https://communities.vmware.com/thread/576209
@@ -221,7 +221,7 @@ func (vm *VM) ChangeCPUCount(virtualCpuCount int) (Task, error) {
 	return vm.ChangeCPUCountWithCore(virtualCpuCount, nil)
 }
 
-// Sets number of available virtual logical processors
+// ChangeCPUCountWithCore sets number of available virtual logical processors
 // (i.e. CPUs x cores per socket) and cores per socket.
 // Socket count is a result of: virtual logical processors/cores per socket
 // https://communities.vmware.com/thread/576209
@@ -394,8 +394,8 @@ func (vm *VM) ChangeMemorySize(size int) (Task, error) {
 		types.MimeRasdItem, "error changing memory size: %s", newMem)
 }
 
-func (vm *VM) RunCustomizationScript(computername, script string) (Task, error) {
-	return vm.Customize(computername, script, false)
+func (vm *VM) RunCustomizationScript(computerName, script string) (Task, error) {
+	return vm.Customize(computerName, script, false)
 }
 
 // GetGuestCustomizationStatus retrieves guest customization status.
@@ -446,7 +446,7 @@ func (vm *VM) BlockWhileGuestCustomizationStatus(unwantedStatus string, timeOutA
 // Customize function allows to set ComputerName, apply customization script and enable or disable the changeSid option
 //
 // Deprecated: Use vm.SetGuestCustomizationSection()
-func (vm *VM) Customize(computername, script string, changeSid bool) (Task, error) {
+func (vm *VM) Customize(computerName, script string, changeSid bool) (Task, error) {
 	err := vm.Refresh()
 	if err != nil {
 		return Task{}, fmt.Errorf("error refreshing VM before running customization: %s", err)
@@ -461,7 +461,7 @@ func (vm *VM) Customize(computername, script string, changeSid bool) (Task, erro
 		Type:                types.MimeGuestCustomizationSection,
 		Info:                "Specifies Guest OS Customization Settings",
 		Enabled:             takeBoolPointer(true),
-		ComputerName:        computername,
+		ComputerName:        computerName,
 		CustomizationScript: script,
 		ChangeSid:           takeBoolPointer(changeSid),
 	}
@@ -522,7 +522,7 @@ func (vm *VM) attachOrDetachDisk(diskParams *types.DiskAttachOrDetachParams, rel
 		attachOrDetachDiskLink.Type, "error attach or detach disk: %s", diskParams)
 }
 
-// Attach an independent disk
+// AttachDisk attaches an independent disk
 // Call attachOrDetachDisk with disk and types.RelDiskAttach to attach an independent disk.
 // Please verify the independent disk is not connected to any VM before calling this function.
 // If the independent disk is connected to a VM, the task will be failed.
@@ -539,7 +539,7 @@ func (vm *VM) AttachDisk(diskParams *types.DiskAttachOrDetachParams) (Task, erro
 	return vm.attachOrDetachDisk(diskParams, types.RelDiskAttach)
 }
 
-// Detach an independent disk
+// DetachDisk detaches an independent disk
 // Call attachOrDetachDisk with disk and types.RelDiskDetach to detach an independent disk.
 // Please verify the independent disk is connected the VM before calling this function.
 // If the independent disk is not connected to the VM, the task will be failed.
@@ -556,7 +556,7 @@ func (vm *VM) DetachDisk(diskParams *types.DiskAttachOrDetachParams) (Task, erro
 	return vm.attachOrDetachDisk(diskParams, types.RelDiskDetach)
 }
 
-// Helper function which finds media and calls InsertMedia
+// HandleInsertMedia helper function finds media and calls InsertMedia
 func (vm *VM) HandleInsertMedia(org *Org, catalogName, mediaName string) (Task, error) {
 
 	catalog, err := org.GetCatalogByName(catalogName, false)
@@ -617,7 +617,7 @@ func isMediaInjected(items []*types.VirtualHardwareItem) bool {
 	return false
 }
 
-// Helper function which finds media and calls EjectMedia
+// HandleEjectMedia is a helper function which finds media and calls EjectMedia
 func (vm *VM) HandleEjectMedia(org *Org, catalogName, mediaName string) (EjectTask, error) {
 	catalog, err := org.GetCatalogByName(catalogName, false)
 	if err != nil {
@@ -638,7 +638,7 @@ func (vm *VM) HandleEjectMedia(org *Org, catalogName, mediaName string) (EjectTa
 	return task, err
 }
 
-// Insert media for VM
+// InsertMedia insert media for a VM
 // Call insertOrEjectMedia with media and types.RelMediaInsertMedia to insert media from VM.
 func (vm *VM) InsertMedia(mediaParams *types.MediaInsertOrEjectParams) (Task, error) {
 	util.Logger.Printf("[TRACE] Insert media, HREF: %s\n", mediaParams.Media.HREF)
@@ -651,7 +651,7 @@ func (vm *VM) InsertMedia(mediaParams *types.MediaInsertOrEjectParams) (Task, er
 	return vm.insertOrEjectMedia(mediaParams, types.RelMediaInsertMedia)
 }
 
-// Eject media from VM
+// EjectMedia ejects media from VM
 // Call insertOrEjectMedia with media and types.RelMediaEjectMedia to eject media from VM.
 // If media isn't inserted then task still will be successful.
 func (vm *VM) EjectMedia(mediaParams *types.MediaInsertOrEjectParams) (EjectTask, error) {
@@ -709,7 +709,7 @@ func (vm *VM) insertOrEjectMedia(mediaParams *types.MediaInsertOrEjectParams, li
 		insertOrEjectMediaLink.Type, "error insert or eject media: %s", mediaParams)
 }
 
-// Use the get existing VM question for operation which need additional response
+// GetQuestion uses the get existing VM question for operation which need additional response
 // Reference:
 // https://code.vmware.com/apis/287/vcloud#/doc/doc/operations/GET-VmPendingQuestion.html
 func (vm *VM) GetQuestion() (types.VmPendingQuestion, error) {
@@ -720,15 +720,14 @@ func (vm *VM) GetQuestion() (types.VmPendingQuestion, error) {
 	req := vm.client.NewRequest(map[string]string{}, http.MethodGet, *apiEndpoint, nil)
 
 	resp, err := vm.client.Http.Do(req)
+	if err != nil {
+		return types.VmPendingQuestion{}, fmt.Errorf("error getting VM question: %s", err)
+	}
 
 	// vCD security feature - on no question return 403 access error
 	if http.StatusForbidden == resp.StatusCode {
 		util.Logger.Printf("No question found for VM: %s\n", vm.VM.ID)
 		return types.VmPendingQuestion{}, nil
-	}
-
-	if err != nil {
-		return types.VmPendingQuestion{}, fmt.Errorf("error getting question: %s", err)
 	}
 
 	if http.StatusOK != resp.StatusCode {
@@ -746,7 +745,7 @@ func (vm *VM) GetQuestion() (types.VmPendingQuestion, error) {
 
 }
 
-// Use the provide answer to existing VM question for operation which need additional response
+// AnswerQuestion uses the provided answer to existing VM question for operation which need additional response
 // Reference:
 // https://code.vmware.com/apis/287/vcloud#/doc/doc/operations/POST-AnswerVmPendingQuestion.html
 func (vm *VM) AnswerQuestion(questionId string, choiceId int) error {

commit 3d33f2f26671b79d81039c339194a8ab6b256ef9

Fix client name that was broken in PR #379
Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

govcd/api.go
@@ -226,8 +226,8 @@ func (client *Client) newRequest(params map[string]string, notEncodedParams map[
 			}
 		}
 	}
-	if cli.customHeader != nil {
-		for k, v := range cli.customHeader {
+	if client.customHeader != nil {
+		for k, v := range client.customHeader {
 			for _, v1 := range v {
 				req.Header.Add(k, v1)
 			}