7c45efb2ede8aaeb4ce59b543c466d5d214c6a9d
Dainius Serplis <dserplis@vmware.com>
2021-12-23 09:57:33 +0200

    Tune PR, add changelog record
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- govcd/vm.go
+++ govcd/vm.go
@@ -245,8 +245,7 @@ func (vm *VM) ChangeCPUCountWithCore(virtualCpuCount int, coresPerSocket *int) (
 		Reservation:     0,
 		ResourceType:    types.ResourceTypeProcessor,
 		VirtualQuantity: int64(virtualCpuCount),
-		//Weight:          0,
-		CoresPerSocket: coresPerSocket,
+		CoresPerSocket:  coresPerSocket,
 		Link: &types.Link{
 			HREF: vm.VM.HREF + "/virtualHardwareSection/cpu",
 			Rel:  "edit",
