beb5b323ccec7cd002299b19567f17e3b9a43edc
abarreiro <abarreiro@vmware.com>
2022-03-21 10:05:12 +0100

    Strengthen the test for groups userlist
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- govcd/group_test.go
+++ govcd/group_test.go
@@ -231,6 +231,10 @@ func (vcd *TestVCD) test_GroupUserListIsPopulated(check *C) {
 	check.Assert(user.User.Name, Equals, userName)
 	check.Assert(len(user.User.GroupReferences.GroupReference), Equals, 1)
 
+	// We check here that the user used for update is the same as we had originally, except the user list
+	grp.Group.UsersList = nil
+	check.Assert(copyWithoutUserList(grp.Group), DeepEquals, grp)
+
 	// We check here that groupReferences doesn't make VCD fail, they should be sent as nil
 	err = user.Update()
 	check.Assert(err, IsNil)
