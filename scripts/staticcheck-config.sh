877dfc81c6b8b80431b2fa18ef8b0fb59bf6dc1d
Dainius <Didainius@users.noreply.github.com>
2022-04-04 10:02:41 +0300

    Bump staticcheck to 2022.1 with Go 1.18 support (#457)

--- scripts/staticcheck-config.sh
+++ scripts/staticcheck-config.sh
@@ -1,4 +1,4 @@
 export STATICCHECK_URL=https://github.com/dominikh/go-tools/releases/download
-export STATICCHECK_VERSION=2021.1.1
+export STATICCHECK_VERSION=2022.1
 export STATICCHECK_FILE=staticcheck_linux_amd64.tar.gz
 
