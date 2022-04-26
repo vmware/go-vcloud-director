eebce2c25bceb72def31193725ae07ebae897dac
Miguel Sama <msama@vmware.com>
2022-04-26 12:25:10 +0200

    Add UpdateSecurityTag
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- govcd/nsxt_security_tags.go
+++ govcd/nsxt_security_tags.go
@@ -107,6 +107,48 @@ func (org *Org) GetVMSecurityTags(id string) (*types.EntitySecurityTags, error) 
 	return &entitySecurityTags, nil
 }
 
-func UpdateSecurityTag() {}
+// UpdateSecurityTag updates the entities associated with a Security Tag.
+// Only the list of tagged entities can be updated. The name cannot be updated.
+// Any other existing entities not in the list will be untagged.
+func (org *Org) UpdateSecurityTag(securityTag *types.SecurityTag) error {
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointSecurityTags
+	apiVersion, err := org.client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return err
+	}
 
-func UpdateVMTags() {}
+	urlRef, err := org.client.OpenApiBuildEndpoint(endpoint, "/tag")
+	if err != nil {
+		return err
+	}
+
+	err = org.client.OpenApiPutItem(apiVersion, urlRef, nil, securityTag, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateVMSecurityTags updates the list of tags for a specific VM. An empty list of tags means to delete all dags
+// for the VM. If user has edit permission on the VM, user can edit its tags.
+func (org *Org) UpdateVMSecurityTags(id string, entitySecurityTags *types.EntitySecurityTags) (*types.EntitySecurityTags, error) {
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointSecurityTags
+	apiVersion, err := org.client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	urlRef, err := org.client.OpenApiBuildEndpoint(endpoint, fmt.Sprintf("/vm/%s", id))
+	if err != nil {
+		return nil, err
+	}
+
+	var serverEntitySecurityTags types.EntitySecurityTags
+	err = org.client.OpenApiPutItem(apiVersion, urlRef, nil, entitySecurityTags, &serverEntitySecurityTags, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &serverEntitySecurityTags, nil
+}
