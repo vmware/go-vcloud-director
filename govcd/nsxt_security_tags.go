75d2dc7e52d170b9e31da8cadb1f2dce6498eb55
Miguel Sama <msama@vmware.com>
2022-04-26 10:33:10 +0200

    Add GetSecurityTagValues
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- govcd/nsxt_security_tags.go
+++ govcd/nsxt_security_tags.go
@@ -8,7 +8,7 @@ import (
 )
 
 // GetSecurityTaggedEntities Retrieves the list of entities that have at least one tag assigned to it.
-// Besides entityType, additional supported filters are:
+// Besides, entityType, additional supported filters are:
 //   - tag - The tag to search by. I.e: filter=(tag==Web;entityType==vm)
 func (org *Org) GetSecurityTaggedEntities(filter string) ([]*types.SecurityTaggedEntity, error) {
 	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointSecurityTags
@@ -42,7 +42,48 @@ func (org *Org) GetSecurityTaggedEntities(filter string) ([]*types.SecurityTagge
 	return securityTaggedEntities, nil
 }
 
-func GetTagValues() {}
+// Retrieves the list of security tags that are in the organization and can be reused to tag an entity.
+// The list of tags include tags assigned to entities within the organization.
+//This API is meant for organization user only (i.e. not system provider).
+func (org *Org) GetSecurityTagValues(filter string) ([]*types.SecurityTagValue, error) {
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointSecurityTags
+	apiVersion, err := org.client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	urlRef, err := org.client.OpenApiBuildEndpoint(endpoint, "/values")
+	if err != nil {
+		return nil, err
+	}
+
+	// If sysadmin, getting the org context and this method only works for org users
+	orgContextHeaders := make(map[string]string)
+	if org.client.IsSysAdmin {
+		orgContextHeaders["X-VMWARE-VCLOUD-AUTH-CONTEXT"] = org.Org.Name
+		orgContextHeaders["X-VMWARE-VCLOUD-TENANT-CONTEXT"] = org.Org.ID
+	}
+
+	v := url.Values{}
+	v.Set("filter", filter)
+	rawValues, err := org.client.openApiGetAllPages(apiVersion, urlRef, v, nil, nil, orgContextHeaders)
+	if err != nil {
+		return nil, err
+	}
+
+	securityTaggedValues := make([]*types.SecurityTagValue, len(rawValues))
+
+	for i, k := range rawValues {
+		var temp types.SecurityTagValue
+		err = json.Unmarshal(k, &temp)
+		if err != nil {
+			return nil, fmt.Errorf("error when unmarshalling SecurityTaggedEntity - %s", err)
+		}
+		securityTaggedValues[i] = &temp
+	}
+
+	return securityTaggedValues, nil
+}
 
 func GetVMTags() {}
 
