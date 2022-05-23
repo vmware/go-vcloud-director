3a25b6b17bd6d48cd26320ac956ff07c8a43c3c3
Dainius <Didainius@users.noreply.github.com>
2022-05-23 14:22:24 +0300

    Add more convenience methods for Org Vdc network DHCP handling (#469)
    Added a few functions for the convenience of handling Org network DHCP which are especially covenient when dealing with Org Networks in VDC Groups:
    OpenApiOrgVdcNetwork.GetOpenApiOrgVdcNetworkDhcp, OpenApiOrgVdcNetwork.DeletNetworkDhcp
    OpenApiOrgVdcNetwork.UpdateDhcp

--- govcd/openapi_org_network_dhcp.go
+++ govcd/openapi_org_network_dhcp.go
@@ -18,6 +18,41 @@ type OpenApiOrgVdcNetworkDhcp struct {
 }
 
 // GetOpenApiOrgVdcNetworkDhcp allows to retrieve DHCP configuration for specific Org VDC network
+func (orgVdcNet *OpenApiOrgVdcNetwork) GetOpenApiOrgVdcNetworkDhcp() (*OpenApiOrgVdcNetworkDhcp, error) {
+	if orgVdcNet == nil || orgVdcNet.client == nil {
+		return nil, fmt.Errorf("error ")
+	}
+
+	client := orgVdcNet.client
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointOrgVdcNetworksDhcp
+	apiVersion, err := client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	if orgVdcNet.OpenApiOrgVdcNetwork.ID == "" {
+		return nil, fmt.Errorf("empty Org VDC network ID")
+	}
+
+	urlRef, err := client.OpenApiBuildEndpoint(fmt.Sprintf(endpoint, orgVdcNet.OpenApiOrgVdcNetwork.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	orgNetDhcp := &OpenApiOrgVdcNetworkDhcp{
+		OpenApiOrgVdcNetworkDhcp: &types.OpenApiOrgVdcNetworkDhcp{},
+		client:                   client,
+	}
+
+	err = client.OpenApiGetItem(apiVersion, urlRef, nil, orgNetDhcp.OpenApiOrgVdcNetworkDhcp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return orgNetDhcp, nil
+}
+
+// GetOpenApiOrgVdcNetworkDhcp allows to retrieve DHCP configuration for specific Org VDC network
 // ID specified as orgNetworkId using OpenAPI
 func (vdc *Vdc) GetOpenApiOrgVdcNetworkDhcp(orgNetworkId string) (*OpenApiOrgVdcNetworkDhcp, error) {
 
@@ -57,40 +92,7 @@ func (vdc *Vdc) GetOpenApiOrgVdcNetworkDhcp(orgNetworkId string) (*OpenApiOrgVdc
 // UpdateOpenApiOrgVdcNetworkDhcp allows to update DHCP configuration for specific Org VDC network
 // ID specified as orgNetworkId using OpenAPI
 func (vdc *Vdc) UpdateOpenApiOrgVdcNetworkDhcp(orgNetworkId string, orgVdcNetworkDhcpConfig *types.OpenApiOrgVdcNetworkDhcp) (*OpenApiOrgVdcNetworkDhcp, error) {
-	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointOrgVdcNetworksDhcp
-	apiVersion, err := vdc.client.getOpenApiHighestElevatedVersion(endpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	urlRef, err := vdc.client.OpenApiBuildEndpoint(fmt.Sprintf(endpoint, orgNetworkId))
-	if err != nil {
-		return nil, err
-	}
-
-	orgNetDhcpResponse := &OpenApiOrgVdcNetworkDhcp{
-		OpenApiOrgVdcNetworkDhcp: &types.OpenApiOrgVdcNetworkDhcp{},
-		client:                   vdc.client,
-	}
-
-	// From v35.0 onwards, if orgVdcNetworkDhcpConfig.LeaseTime or orgVdcNetworkDhcpConfig.Mode are not explicitly
-	// passed, the API doesn't use any defaults returning an error. Previous API versions were setting
-	// LeaseTime to 86400 seconds and Mode to EDGE if these values were not supplied. These two conditional
-	// address the situation.
-	if orgVdcNetworkDhcpConfig.LeaseTime == nil {
-		orgVdcNetworkDhcpConfig.LeaseTime = takeIntAddress(86400)
-	}
-
-	if len(orgVdcNetworkDhcpConfig.Mode) == 0 {
-		orgVdcNetworkDhcpConfig.Mode = "EDGE"
-	}
-
-	err = vdc.client.OpenApiPutItem(apiVersion, urlRef, nil, orgVdcNetworkDhcpConfig, orgNetDhcpResponse.OpenApiOrgVdcNetworkDhcp, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error updating Org VDC network DHCP configuration: %s", err)
-	}
-
-	return orgNetDhcpResponse, nil
+	return updateOrgNetworkDhcp(vdc.client, orgNetworkId, orgVdcNetworkDhcpConfig)
 }
 
 // DeleteOpenApiOrgVdcNetworkDhcp allows to perform HTTP DELETE request on DHCP pool configuration for specified Org VDC
@@ -118,4 +120,67 @@ func (vdc *Vdc) DeleteOpenApiOrgVdcNetworkDhcp(orgNetworkId string) error {
 	}
 
 	return nil
+}
+
+// DeletNetworkDhcp allows to perform HTTP DELETE request on DHCP pool configuration for Org network
+func (orgVdcNet *OpenApiOrgVdcNetwork) DeletNetworkDhcp() error {
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointOrgVdcNetworksDhcp
+	apiVersion, err := orgVdcNet.client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return err
+	}
+
+	if orgVdcNet.OpenApiOrgVdcNetwork.ID == "" {
+		return fmt.Errorf("cannot delete Org VDC network DHCP configuration without ID")
+	}
+
+	urlRef, err := orgVdcNet.client.OpenApiBuildEndpoint(fmt.Sprintf(endpoint, orgVdcNet.OpenApiOrgVdcNetwork.ID))
+	if err != nil {
+		return err
+	}
+
+	err = orgVdcNet.client.OpenApiDeleteItem(apiVersion, urlRef, nil, nil)
+
+	if err != nil {
+		return fmt.Errorf("error deleting Org VDC network DHCP configuration: %s", err)
+	}
+
+	return nil
+}
+
+func updateOrgNetworkDhcp(client *Client, orgNetworkId string, orgVdcNetworkDhcpConfig *types.OpenApiOrgVdcNetworkDhcp) (*OpenApiOrgVdcNetworkDhcp, error) {
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointOrgVdcNetworksDhcp
+	apiVersion, err := client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	urlRef, err := client.OpenApiBuildEndpoint(fmt.Sprintf(endpoint, orgNetworkId))
+	if err != nil {
+		return nil, err
+	}
+
+	orgNetDhcpResponse := &OpenApiOrgVdcNetworkDhcp{
+		OpenApiOrgVdcNetworkDhcp: &types.OpenApiOrgVdcNetworkDhcp{},
+		client:                   client,
+	}
+
+	// From v35.0 onwards, if orgVdcNetworkDhcpConfig.LeaseTime or orgVdcNetworkDhcpConfig.Mode are not explicitly
+	// passed, the API doesn't use any defaults returning an error. Previous API versions were setting
+	// LeaseTime to 86400 seconds and Mode to EDGE if these values were not supplied. These two conditional
+	// address the situation.
+	if orgVdcNetworkDhcpConfig.LeaseTime == nil {
+		orgVdcNetworkDhcpConfig.LeaseTime = takeIntAddress(86400)
+	}
+
+	if len(orgVdcNetworkDhcpConfig.Mode) == 0 {
+		orgVdcNetworkDhcpConfig.Mode = "EDGE"
+	}
+
+	err = client.OpenApiPutItem(apiVersion, urlRef, nil, orgVdcNetworkDhcpConfig, orgNetDhcpResponse.OpenApiOrgVdcNetworkDhcp, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error updating Org VDC network DHCP configuration: %s", err)
+	}
+
+	return orgNetDhcpResponse, nil
 }
