00916403b47ee4fb3195cbe2bb5cae5b5ab0347c
Miguel Sama <msama@vmware.com>
2022-05-25 18:03:42 +0200

    Add test
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- govcd/nsxt_route_advertisement_test.go
+++ govcd/nsxt_route_advertisement_test.go
@@ -1,5 +1,53 @@
+//go:build network || nsxt || functional || openapi || ALL
+// +build network nsxt functional openapi ALL
+
 /*
  * Copyright 2022 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
  */
 
 package govcd
+
+import (
+	"fmt"
+	"github.com/vmware/go-vcloud-director/v2/types/v56"
+	. "gopkg.in/check.v1"
+)
+
+func (vcd *TestVCD) Test_NsxtEdgeRouteAdvertisement(check *C) {
+	skipNoNsxtConfiguration(vcd, check)
+	skipOpenApiEndpointTest(vcd, check, types.OpenApiPathVersion1_0_0+types.OpenApiEndpointNsxtRouteAdvertisement)
+
+	org, err := vcd.client.GetOrgByName(vcd.config.VCD.Org)
+	check.Assert(err, IsNil)
+	nsxtVdc, err := org.GetVDCByName(vcd.config.VCD.Nsxt.Vdc, false)
+	check.Assert(err, IsNil)
+	edge, err := nsxtVdc.GetNsxtEdgeGatewayByName(vcd.config.VCD.Nsxt.EdgeGateway)
+	check.Assert(err, IsNil)
+
+	// Make sure we are using a dedicated Tier-0 gateway (otherwise route advertisement won't be available)
+	edge.EdgeGateway.EdgeGatewayUplinks[0].Dedicated = true
+	edge, err = edge.Update(edge.EdgeGateway)
+	check.Assert(err, IsNil)
+	check.Assert(edge, NotNil)
+
+	network1 := "192.168.1.0/24"
+	network2 := "192.168.2.0/24"
+	networksToAdvertise := []string{network1, network2} // Sample networks to advertise
+
+	nsxtEdgeRouteAdvertisement, err := edge.UpdateNsxtRouteAdvertisement(true, networksToAdvertise, true)
+	check.Assert(err, IsNil)
+	check.Assert(nsxtEdgeRouteAdvertisement, NotNil)
+	check.Assert(nsxtEdgeRouteAdvertisement.Enable, Equals, true)
+	check.Assert(nsxtEdgeRouteAdvertisement.Subnets, Equals, true)
+	check.Assert(checkNetworkInSubnetsSlice(network1, networksToAdvertise), IsNil)
+	check.Assert(checkNetworkInSubnetsSlice(network2, networksToAdvertise), IsNil)
+}
+
+func checkNetworkInSubnetsSlice(network string, subnets []string) error {
+	for _, subnet := range subnets {
+		if subnet == network {
+			return nil
+		}
+	}
+	return fmt.Errorf("network %s is not within the slice provided", network)
+}
