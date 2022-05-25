9b0fd54a8ddb440d10cc642ff948b6352a892dbc
Miguel Sama <msama@vmware.com>
2022-05-25 17:17:39 +0200

    Finish writing Route advertisement CRUD methods
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- govcd/nsxt_route_advertisement.go
+++ govcd/nsxt_route_advertisement.go
@@ -11,7 +11,12 @@ import (
 
 // GetNsxtRouteAdvertisement retrieves the list of subnets that will be advertised so that the Edge Gateway can route
 // out to the connected external network.
-func (egw *NsxtEdgeGateway) GetNsxtRouteAdvertisement() (*types.RouteAdvertisement, error) {
+func (egw *NsxtEdgeGateway) GetNsxtRouteAdvertisement(useTenantContext bool) (*types.RouteAdvertisement, error) {
+	err := checkSanityNsxtEdgeGatewayRouteAdvertisement(egw)
+	if err != nil {
+		return nil, err
+	}
+
 	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointNsxtRouteAdvertisement
 
 	highestApiVersion, err := egw.client.getOpenApiHighestElevatedVersion(endpoint)
@@ -24,22 +29,84 @@ func (egw *NsxtEdgeGateway) GetNsxtRouteAdvertisement() (*types.RouteAdvertiseme
 		return nil, err
 	}
 
-	routeAdvertisement := &types.RouteAdvertisement{}
-	err = egw.client.OpenApiGetItem(highestApiVersion, urlRef, nil, routeAdvertisement, nil)
+	var tenantContextHeaders map[string]string
+	if useTenantContext {
+		tenantContext, err := egw.getTenantContext()
+		if err != nil {
+			return nil, err
+		}
 
-	return nil, nil
+		tenantContextHeaders = getTenantContextHeader(tenantContext)
+	}
+
+	routeAdvertisement := &types.RouteAdvertisement{}
+	err = egw.client.OpenApiGetItem(highestApiVersion, urlRef, nil, routeAdvertisement, tenantContextHeaders)
+	if err != nil {
+		return nil, err
+	}
+
+	return routeAdvertisement, nil
 }
 
 // UpdateNsxtRouteAdvertisement updates the list of subnets that will be advertised so that the Edge Gateway can route
 // out to the connected external network.
-func (egw *NsxtEdgeGateway) UpdateNsxtRouteAdvertisement(enable bool, subnets []string) (*types.RouteAdvertisement, error) {
-	return nil, nil
+func (egw *NsxtEdgeGateway) UpdateNsxtRouteAdvertisement(enable bool, subnets []string, useTenantContext bool) (*types.RouteAdvertisement, error) {
+	err := checkSanityNsxtEdgeGatewayRouteAdvertisement(egw)
+	if err != nil {
+		return nil, err
+	}
+
+	endpoint := types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointNsxtRouteAdvertisement
+
+	highestApiVersion, err := egw.client.getOpenApiHighestElevatedVersion(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	urlRef, err := egw.client.OpenApiBuildEndpoint(fmt.Sprintf(endpoint, egw.EdgeGateway.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	var tenantContextHeaders map[string]string
+	if useTenantContext {
+		tenantContext, err := egw.getTenantContext()
+		if err != nil {
+			return nil, err
+		}
+
+		tenantContextHeaders = getTenantContextHeader(tenantContext)
+	}
+
+	routeAdvertisement := &types.RouteAdvertisement{
+		Enable:  enable,
+		Subnets: subnets,
+	}
+
+	err = egw.client.OpenApiPutItem(highestApiVersion, urlRef, nil, routeAdvertisement, nil, tenantContextHeaders)
+	if err != nil {
+		return nil, err
+	}
+
+	return egw.GetNsxtRouteAdvertisement(useTenantContext)
 }
 
 // DeleteNsxtRouteAdvertisement deletes the list of subnets that will be advertised.
-func (egw *NsxtEdgeGateway) DeleteNsxtRouteAdvertisement() error {
-	return nil
+func (egw *NsxtEdgeGateway) DeleteNsxtRouteAdvertisement(useTenantContext bool) error {
+	_, err := egw.UpdateNsxtRouteAdvertisement(false, []string{}, useTenantContext)
+	return err
 }
 
-// checkSanityNsxtEdgeGatewayRouteAdvertisement
-func checkSanityNsxtEdgeGatewayRouteAdvertisement(vdc *Vdc) error {}
+// checkSanityNsxtEdgeGatewayRouteAdvertisement function performs some checks to *NsxtEdgeGateway parameter and returns error
+// if something is wrong. It is useful with methods NsxtEdgeGateway.[Get/Update/Delete]NsxtRouteAdvertisement
+func checkSanityNsxtEdgeGatewayRouteAdvertisement(egw *NsxtEdgeGateway) error {
+	if egw.EdgeGateway == nil {
+		return fmt.Errorf("the EdgeGateway pointer is nil. Please initialize it first before using this method")
+	}
+
+	if egw.EdgeGateway.ID == "" {
+		return fmt.Errorf("the EdgeGateway ID is empty. Please initialize it first before using this method")
+	}
+
+	return nil
+}
