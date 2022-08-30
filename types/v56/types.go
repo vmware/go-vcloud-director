5dd6d8f545eb1be3df960c43591af4e5c2748b65
thomas.moreau <thomas.moreau@fr.clara.net>
2022-08-30 12:34:54 +0200

    fix: changing Name of field 'Hypervisor' to 'HostName' in QueryResultVMRecordType struct in orderto match XML field name

--- types/v56/types.go
+++ types/v56/types.go
@@ -2381,7 +2381,7 @@ type QueryResultVMRecordType struct {
 	DateCreated              string    `xml:"dateCreated,attr,omitempty"`
 	TotalStorageAllocatedMb  string    `xml:"totalStorageAllocatedMb,attr,omitempty"`
 	IsExpired                bool      `xml:"isExpired,attr,omitempty"`
-	Hypervisor               string    `xml:"hostName,attr,omitempty"` // HostName/hypervisor of virtual machine
+	HostName                 string    `xml:"hostName,attr,omitempty"` // HostName=Hypervisor of virtual machine
 	Link                     []*Link   `xml:"Link,omitempty"`
 	MetaData                 *Metadata `xml:"Metadata,omitempty"`
 }
