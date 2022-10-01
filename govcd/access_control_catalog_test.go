759860fc90dce5fbc5af84c25150e64892850267
Giuseppe Maxia <gmaxia@vmware.com>
2022-10-01 21:29:27 +0200

    Improve testCatalogAccessControl
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- govcd/access_control_catalog_test.go
+++ govcd/access_control_catalog_test.go
@@ -2,7 +2,7 @@
 // +build functional catalog ALL
 
 /*
- * Copyright 2020 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
+ * Copyright 2022 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
  */
 
 package govcd
@@ -17,7 +17,7 @@ import (
 )
 
 // catalogTenantContext defines whether we use tenant context during catalog tests.
-// By default is off, unless variable VCD_CATALOG_TENANT_CONTEXT is set
+// By default, is off, unless variable VCD_CATALOG_TENANT_CONTEXT is set
 var catalogTenantContext = os.Getenv("VCD_CATALOG_TENANT_CONTEXT") != ""
 
 // GetId completes the implementation of interface accessControlType
@@ -94,14 +94,20 @@ func (vcd *TestVCD) Test_CatalogAccessControl(check *C) {
 
 func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessControlType, testName, catalogName string, check *C) {
 
-	var users = []struct {
+	type testUser struct {
 		name string
 		role string
 		user *OrgUser
-	}{
-		{"ac-user1", OrgUserRoleVappAuthor, nil},
-		{"ac-user2", OrgUserRoleOrganizationAdministrator, nil},
-		{"ac-user3", OrgUserRoleCatalogAuthor, nil},
+	}
+	var sameOrgUsers = []testUser{
+		{"ac-vapp-author", OrgUserRoleVappAuthor, nil},
+		{"ac-org-admin", OrgUserRoleOrganizationAdministrator, nil},
+		{"ac-cat-author", OrgUserRoleCatalogAuthor, nil},
+	}
+	var newOrgUsers = []testUser{
+		{"ac-new-vapp-author", OrgUserRoleVappAuthor, nil},
+		{"ac-new-org-admin", OrgUserRoleOrganizationAdministrator, nil},
+		{"ac-new-cat-author", OrgUserRoleCatalogAuthor, nil},
 	}
 
 	orgName := "ac-testorg"
@@ -126,8 +132,31 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 			err = newOrg.Delete(true, true)
 			check.Assert(err, IsNil)
 		}()
+		// Create three users in the new org
+		for i := 0; i < len(newOrgUsers); i++ {
+			newOrgUsers[i].user, err = newOrg.CreateUserSimple(OrgUserConfiguration{
+				Name: newOrgUsers[i].name, Password: newOrgUsers[i].name, RoleName: newOrgUsers[i].role, IsEnabled: true,
+			})
+			check.Assert(err, IsNil)
+			check.Assert(newOrgUsers[i].user, NotNil)
+			// No need to add this user to clean-up list. It will be removed when its org is deleted
+		}
 	}
 
+	checkAllUsersAccess := func(label string) {
+		if !testVerbose {
+			return
+		}
+		fmt.Printf("[checkAllUsersAccess] %s\n", label)
+		for _, user := range sameOrgUsers {
+			err := testAccessToSharedCatalog(adminOrg, catalogName, user.name, user.name)
+			check.Assert(err, IsNil)
+		}
+		for _, user := range newOrgUsers {
+			err := testAccessToSharedCatalog(newOrg, catalogName, user.name, user.name)
+			check.Assert(err, IsNil)
+		}
+	}
 	checkEmpty := func() {
 		settings, err := catalog.GetAccessControl(catalogTenantContext)
 		check.Assert(err, IsNil)
@@ -135,32 +164,37 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 		check.Assert(settings.AccessSettings, IsNil)             // There should not be any explicit sharing
 	}
 
-	// Create three users
-	for i := 0; i < len(users); i++ {
-		users[i].user, err = adminOrg.CreateUserSimple(OrgUserConfiguration{
-			Name: users[i].name, Password: users[i].name, RoleName: users[i].role, IsEnabled: true,
+	// Create three users in the same org
+	for i := 0; i < len(sameOrgUsers); i++ {
+		sameOrgUsers[i].user, err = adminOrg.CreateUserSimple(OrgUserConfiguration{
+			Name: sameOrgUsers[i].name, Password: sameOrgUsers[i].name, RoleName: sameOrgUsers[i].role, IsEnabled: true,
 		})
 		check.Assert(err, IsNil)
-		check.Assert(users[i].user, NotNil)
-		AddToCleanupList(users[i].name, "user", vcd.config.VCD.Org, testName)
+		check.Assert(sameOrgUsers[i].user, NotNil)
+		AddToCleanupList(sameOrgUsers[i].name, "user", vcd.config.VCD.Org, testName)
 	}
+
 	defer func() {
-		for i := 0; i < len(users); i++ {
+		for i := 0; i < len(sameOrgUsers); i++ {
 			if testVerbose {
-				fmt.Printf("deleting %s\n", users[i].name)
+				fmt.Printf("deleting %s\n", sameOrgUsers[i].name)
 			}
-			err = users[i].user.Delete(false)
+			err = sameOrgUsers[i].user.Delete(false)
 			check.Assert(err, IsNil)
 		}
 	}()
 	checkEmpty()
-	//globalSettings := types.ControlAccessParams{
-	//	IsSharedToEveryone:  true,
-	//	EveryoneAccessLevel: takeStringPointer(types.ControlAccessReadWrite),
-	//	AccessSettings: nil,
-	//}
-	//err = testAccessControl(catalogName+" catalog global", catalog, globalSettings, globalSettings, true, check)
-	//check.Assert(err, IsNil)
+	checkAllUsersAccess("empty")
+	globalSettings := types.ControlAccessParams{
+		IsSharedToEveryone:  true,
+		EveryoneAccessLevel: takeStringPointer(types.ControlAccessReadOnly),
+		AccessSettings:      nil,
+	}
+	err = testAccessControl(catalogName+" catalog global", catalog, globalSettings, globalSettings, true, catalogTenantContext, check)
+	check.Assert(err, IsNil)
+
+	// All users on all orgs should be able to see the catalog
+	checkAllUsersAccess("shared to everyone")
 
 	// Set control access to one user
 	oneUserSettings := types.ControlAccessParams{
@@ -170,9 +204,9 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 			[]*types.AccessSetting{
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[0].user.User.Href,
-						Name: users[0].user.User.Name,
-						Type: users[0].user.User.Type,
+						HREF: sameOrgUsers[0].user.User.Href,
+						Name: sameOrgUsers[0].user.User.Name,
+						Type: sameOrgUsers[0].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessReadWrite,
@@ -183,7 +217,33 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 	err = testAccessControl(catalogName+" catalog one user", catalog, oneUserSettings, oneUserSettings, true, catalogTenantContext, check)
 	check.Assert(err, IsNil)
 
-	// Check that vapp.GetAccessControl and vdc.GetVappAccessControl return the same data
+	checkAllUsersAccess("one user - same org")
+
+	// Set control access to one user on a different org
+	oneUserOtherOrgSettings := types.ControlAccessParams{
+		IsSharedToEveryone:  false,
+		EveryoneAccessLevel: nil,
+		AccessSettings: &types.AccessSettingList{
+			[]*types.AccessSetting{
+				&types.AccessSetting{
+					Subject: &types.LocalSubject{
+						HREF: newOrgUsers[1].user.User.Href,
+						Name: newOrgUsers[1].user.User.Name,
+						Type: newOrgUsers[1].user.User.Type,
+					},
+					ExternalSubject: nil,
+					AccessLevel:     types.ControlAccessReadWrite,
+				},
+			},
+		},
+	}
+	// sharing with a user of a different Org is not allowed: we expect a failure
+	err = testAccessControl(catalogName+" catalog one user - other org", catalog, oneUserOtherOrgSettings, oneUserOtherOrgSettings, true, catalogTenantContext, check)
+	check.Assert(err, NotNil)
+	check.Assert(err.Error(), Matches, `.*Sharing is only supported in an organization.*`)
+	checkAllUsersAccess("one user - other org")
+
+	// Check that catalog.GetAccessControl and vdc.GetCatalogAccessControl return the same data
 	controlAccess, err := catalog.GetAccessControl(catalogTenantContext)
 	check.Assert(err, IsNil)
 	orgControlAccessByName, err := adminOrg.GetCatalogAccessControl(catalogName, catalogTenantContext)
@@ -202,18 +262,18 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 			[]*types.AccessSetting{
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[0].user.User.Href,
+						HREF: sameOrgUsers[0].user.User.Href,
 						//Name: users[0].user.User.Name, // Pass info without name for one of the subjects
-						Type: users[0].user.User.Type,
+						Type: sameOrgUsers[0].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessReadOnly,
 				},
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[1].user.User.Href,
-						Name: users[1].user.User.Name,
-						Type: users[1].user.User.Type,
+						HREF: sameOrgUsers[1].user.User.Href,
+						Name: sameOrgUsers[1].user.User.Name,
+						Type: sameOrgUsers[1].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessFullControl,
@@ -223,6 +283,8 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 	}
 	err = testAccessControl(catalogName+" catalog two users", catalog, twoUserSettings, twoUserSettings, true, catalogTenantContext, check)
 	check.Assert(err, IsNil)
+
+	checkAllUsersAccess("two users - same org")
 
 	// Check removal of sharing setting
 	err = catalog.RemoveAccessControl(catalogTenantContext)
@@ -237,27 +299,27 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 			[]*types.AccessSetting{
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[0].user.User.Href,
-						Name: users[0].user.User.Name,
-						Type: users[0].user.User.Type,
+						HREF: sameOrgUsers[0].user.User.Href,
+						Name: sameOrgUsers[0].user.User.Name,
+						Type: sameOrgUsers[0].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessReadOnly,
 				},
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[1].user.User.Href,
+						HREF: sameOrgUsers[1].user.User.Href,
 						//Name: users[1].user.User.Name,// Pass info without name for one of the subjects
-						Type: users[1].user.User.Type,
+						Type: sameOrgUsers[1].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessFullControl,
 				},
 				&types.AccessSetting{
 					Subject: &types.LocalSubject{
-						HREF: users[2].user.User.Href,
-						Name: users[2].user.User.Name,
-						Type: users[2].user.User.Type,
+						HREF: sameOrgUsers[2].user.User.Href,
+						Name: sameOrgUsers[2].user.User.Name,
+						Type: sameOrgUsers[2].user.User.Type,
 					},
 					ExternalSubject: nil,
 					AccessLevel:     types.ControlAccessReadWrite,
@@ -267,6 +329,7 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 	}
 	err = testAccessControl(catalogName+" catalog three users", catalog, threeUserSettings, threeUserSettings, true, catalogTenantContext, check)
 	check.Assert(err, IsNil)
+	checkAllUsersAccess("three users - same org")
 
 	if vcd.client.Client.IsSysAdmin && newOrg != nil {
 
@@ -278,27 +341,27 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 				[]*types.AccessSetting{
 					&types.AccessSetting{
 						Subject: &types.LocalSubject{
-							HREF: users[0].user.User.Href,
-							Name: users[0].user.User.Name,
-							Type: users[0].user.User.Type,
+							HREF: sameOrgUsers[0].user.User.Href,
+							Name: sameOrgUsers[0].user.User.Name,
+							Type: sameOrgUsers[0].user.User.Type,
 						},
 						ExternalSubject: nil,
 						AccessLevel:     types.ControlAccessReadOnly,
 					},
 					&types.AccessSetting{
 						Subject: &types.LocalSubject{
-							HREF: users[1].user.User.Href,
+							HREF: sameOrgUsers[1].user.User.Href,
 							//Name: users[1].user.User.Name,// Pass info without name for one of the subjects
-							Type: users[1].user.User.Type,
+							Type: sameOrgUsers[1].user.User.Type,
 						},
 						ExternalSubject: nil,
 						AccessLevel:     types.ControlAccessFullControl,
 					},
 					&types.AccessSetting{
 						Subject: &types.LocalSubject{
-							HREF: users[2].user.User.Href,
-							Name: users[2].user.User.Name,
-							Type: users[2].user.User.Type,
+							HREF: sameOrgUsers[2].user.User.Href,
+							Name: sameOrgUsers[2].user.User.Name,
+							Type: sameOrgUsers[2].user.User.Type,
 						},
 						ExternalSubject: nil,
 						AccessLevel:     types.ControlAccessReadWrite,
@@ -317,6 +380,7 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 		}
 		err = testAccessControl(catalogName+" catalog three users and org", catalog, threeUserOrgSettings, threeUserOrgSettings, true, catalogTenantContext, check)
 		check.Assert(err, IsNil)
+		checkAllUsersAccess("three users + one org ")
 
 		err = catalog.RemoveAccessControl(catalogTenantContext)
 		check.Assert(err, IsNil)
@@ -351,6 +415,7 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 		}
 		err = testAccessControl(catalogName+" catalog two org", catalog, twoOrgsSettings, twoOrgsSettings, true, catalogTenantContext, check)
 		check.Assert(err, IsNil)
+		checkAllUsersAccess("two orgs ")
 	}
 
 	// Set empty settings explicitly
@@ -362,4 +427,34 @@ func (vcd *TestVCD) testCatalogAccessControl(adminOrg *AdminOrg, catalog accessC
 
 	checkEmpty()
 
+}
+
+// testAccessToSharedCatalog is an informational tool to show whether a given user can access a shared catalog
+// Its output is visible when -vcd-verbose is used on the command line
+func testAccessToSharedCatalog(org *AdminOrg, catalogName, userName, password string) error {
+	vcdClient := NewVCDClient(org.client.VCDHREF, true)
+	err := vcdClient.Authenticate(userName, password, org.AdminOrg.Name)
+	if err != nil {
+		return err
+	}
+	catalogs, err := queryCatalogList(&vcdClient.Client, nil)
+	if err != nil {
+		return fmt.Errorf("error retrieving catalogs as user '%s': %s", userName, err)
+	}
+
+	found := false
+	for _, cat := range catalogs {
+		if catalogName == cat.Name {
+			found = true
+		}
+	}
+
+	if testVerbose {
+		if found {
+			fmt.Printf("++ user %s from org %s can access catalog %s\n", userName, org.AdminOrg.Name, catalogName)
+		} else {
+			fmt.Printf("## user %s from org %s CANNOT access catalog %s\n", userName, org.AdminOrg.Name, catalogName)
+		}
+	}
+	return nil
 }
