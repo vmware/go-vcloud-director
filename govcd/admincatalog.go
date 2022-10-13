92a39fc2f515eba12e7a4b3b2301bf4db9e14cc2
Giuseppe Maxia <gmaxia@vmware.com>
2022-10-13 09:14:43 +0200

    Add functions to subscribe and sync admin catalogs
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- govcd/admincatalog.go
+++ govcd/admincatalog.go
@@ -7,8 +7,12 @@ package govcd
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
+	"github.com/vmware/go-vcloud-director/v2/util"
 )
 
 // AdminCatalog is a admin view of a VMware Cloud Director Catalog
@@ -128,4 +132,277 @@ func (cat *AdminCatalog) PublishToExternalOrganizations(publishExternalCatalog t
 	}
 
 	return err
+}
+
+// CreateCatalogFromSubscriptionAsync creates a new catalog by subscribing to a published catalog
+// Parameter subscription needs to be filled manually
+func (org *AdminOrg) CreateCatalogFromSubscriptionAsync(subscription types.ExternalCatalogSubscription,
+	storageProfiles *types.CatalogStorageProfiles,
+	catalogName, password string, localCopy bool) (*AdminCatalog, error) {
+
+	// If the receiving Org doesn't have any VDCs, it means that there is no storage that can be used
+	// by a catalog
+	if len(org.AdminOrg.Vdcs.Vdcs) == 0 {
+		return nil, fmt.Errorf("org %s does not have any storage to support a catalog", org.AdminOrg.Name)
+	}
+	href := ""
+
+	// The subscribed catalog creation is like a regular catalog creation, with the
+	// difference that the subscription details are filled in
+	for _, link := range org.AdminOrg.Link {
+		if link.Rel == "add" && link.Type == types.MimeAdminCatalog {
+			href = link.HREF
+			break
+		}
+	}
+	if href == "" {
+		return nil, fmt.Errorf("catalog creation link not found for org %s", org.AdminOrg.Name)
+	}
+	reqCatalog := &types.Catalog{
+		Name: catalogName,
+	}
+	adminCatalog := types.AdminCatalog{
+		Xmlns:                  types.XMLNamespaceVCloud,
+		Catalog:                *reqCatalog,
+		CatalogStorageProfiles: storageProfiles,
+		ExternalCatalogSubscription: &types.ExternalCatalogSubscription{
+			LocalCopy:                localCopy,
+			Password:                 password,
+			Location:                 subscription.Location,
+			SubscribeToExternalFeeds: true,
+		},
+	}
+	// The subscription URL returned by the API is in abbreviated form
+	// such as "/vcsp/lib/65637586-c703-48ae-a7e2-82605d18db57/"
+	// If the passed URL is so abbreviated, we need to add the host
+	if !IsValidUrl(subscription.Location) {
+		// Get the catalog base URL
+		cutPosition := strings.Index(org.AdminOrg.HREF, "/api")
+		catalogHost := org.AdminOrg.HREF[:cutPosition]
+		subscriptionUrl, err := url.JoinPath(catalogHost, subscription.Location)
+		if err != nil {
+			return nil, fmt.Errorf("error composing subscription URL: %s", err)
+		}
+		adminCatalog.ExternalCatalogSubscription.Location = subscriptionUrl
+	}
+
+	adminCatalog.ExternalCatalogSubscription.Password = password
+	adminCatalog.ExternalCatalogSubscription.LocalCopy = localCopy
+	_, err := org.client.ExecuteRequest(href, http.MethodPost, types.MimeAdminCatalog,
+		"error subscribing to catalog: %s", adminCatalog, nil)
+	if err != nil {
+		return nil, err
+	}
+	return org.GetAdminCatalogByName(catalogName, true)
+}
+
+// FullSubscriptionUrl returns the subscription URL from a publisher catalog
+// adding the HOST if needed
+func (cat *AdminCatalog) FullSubscriptionUrl() string {
+	if cat.AdminCatalog.PublishExternalCatalogParams == nil {
+		return ""
+	}
+	subscriptionUrl := cat.AdminCatalog.PublishExternalCatalogParams.CatalogPublishedUrl
+	var err error
+	if !IsValidUrl(subscriptionUrl) {
+		// Get the catalog base URL
+		cutPosition := strings.Index(cat.AdminCatalog.HREF, "/api")
+		catalogHost := cat.AdminCatalog.HREF[:cutPosition]
+		subscriptionUrl, err = url.JoinPath(catalogHost, subscriptionUrl)
+		if err != nil {
+			return ""
+		}
+	}
+	return subscriptionUrl
+}
+
+// IsValidUrl returns true if the given URL is complete and usable
+func IsValidUrl(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+// CreateCatalogFromSubscription is a wrapper around CreateCatalogFromSubscriptionAsync
+// After catalog creation, it waits for the import tasks to complete within a given timeout
+func (org *AdminOrg) CreateCatalogFromSubscription(subscription types.ExternalCatalogSubscription,
+	storageProfiles *types.CatalogStorageProfiles,
+	catalogName, password string, localCopy bool, timeout time.Duration) (*AdminCatalog, error) {
+	noTimeout := timeout == 0
+	adminCatalog, err := org.CreateCatalogFromSubscriptionAsync(subscription, storageProfiles, catalogName, password, localCopy)
+	if err != nil {
+		return nil, err
+	}
+	start := time.Now()
+	for noTimeout || time.Since(start) < timeout {
+		if noTimeout {
+			util.Logger.Printf("[TRACE] [CreateCatalogFromSubscription] no timeout given - Elapsed %s", time.Since(start))
+		}
+		err = adminCatalog.Refresh()
+		if err != nil {
+			return nil, err
+		}
+		if ResourceComplete(adminCatalog.AdminCatalog.Tasks) {
+			return adminCatalog, nil
+		}
+	}
+	return nil, fmt.Errorf("adminCatalog %s still not complete after %s", adminCatalog.AdminCatalog.Name, timeout)
+}
+
+// Sync synchronises a subscribed AdminCatalog
+func (cat *AdminCatalog) Sync() error {
+	// if the catalog was not subscribed, return
+	if cat.AdminCatalog.ExternalCatalogSubscription == nil || cat.AdminCatalog.ExternalCatalogSubscription.Location == "" {
+		return nil
+	}
+	// The sync operation is only available for Catalog, not AdminCatalog.
+	// We use the embedded Catalog object for this purpose
+	catalogHref, err := cat.GetCatalogHref()
+	if err != nil || catalogHref == "" {
+		return fmt.Errorf("empty catalog HREF for admin catalog %s", cat.AdminCatalog.Name)
+	}
+	return elementSync(cat.client, catalogHref, "admin catalog")
+}
+
+// LaunchSync starts synchronisation of a subscribed AdminCatalog
+func (cat *AdminCatalog) LaunchSync() (*Task, error) {
+	// if the catalog was not subscribed, return
+	if cat.AdminCatalog.ExternalCatalogSubscription == nil || cat.AdminCatalog.ExternalCatalogSubscription.Location == "" {
+		return nil, nil
+	}
+	// The sync operation is only available for Catalog, not AdminCatalog.
+	// We use the embedded Catalog object for this purpose
+	catalogHref, err := cat.GetCatalogHref()
+	if err != nil || catalogHref == "" {
+		return nil, fmt.Errorf("empty catalog HREF for admin catalog %s", cat.AdminCatalog.Name)
+	}
+	return elementLaunchSync(cat.client, catalogHref, "admin catalog")
+}
+
+// GetCatalogHref retrieves the regular catalog HREF from an admin catalog
+func (cat *AdminCatalog) GetCatalogHref() (string, error) {
+	href := ""
+	for _, link := range cat.AdminCatalog.Link {
+		if link.Rel == "alternate" && link.Type == types.MimeCatalog {
+			href = link.HREF
+			break
+		}
+	}
+	if href == "" {
+		return "", fmt.Errorf("no regular Catalog HREF found for admin Catalog %s", cat.AdminCatalog.Name)
+	}
+	return href, nil
+}
+
+// QueryVappTemplateList returns a list of vApp templates for the given catalog
+func (catalog *AdminCatalog) QueryVappTemplateList() ([]*types.QueryResultVappTemplateType, error) {
+	return queryVappTemplateList(catalog.client, "catalogName", catalog.AdminCatalog.Name)
+}
+
+// QueryMediaList retrieves a list of media items for the Admin Catalog
+func (catalog *AdminCatalog) QueryMediaList() ([]*types.MediaRecordType, error) {
+	return queryMediaList(catalog.client, catalog.AdminCatalog.HREF)
+}
+
+// LaunchSynchronisationVappTemplates starts synchronisation of a list of vApp templates
+func (cat *AdminCatalog) LaunchSynchronisationVappTemplates(nameList []string) ([]*Task, error) {
+	var taskList []*Task
+	for _, element := range nameList {
+		catalogItem, err := cat.QueryCatalogItem(element)
+		if err != nil {
+			return nil, err
+		}
+		task, err := queryResultCatalogItemToCatalogItem(cat.client, catalogItem).LaunchSync()
+		if err != nil {
+			return nil, err
+		}
+		taskList = append(taskList, task)
+	}
+	return taskList, nil
+}
+
+// LaunchSynchronisationAllVappTemplates starts synchronisation of all vApp templates for a given catalog
+func (cat *AdminCatalog) LaunchSynchronisationAllVappTemplates() ([]*Task, error) {
+	vappTemplatesList, err := cat.QueryVappTemplateList()
+	if err != nil {
+		return nil, err
+	}
+	var nameList []string
+	for _, element := range vappTemplatesList {
+		nameList = append(nameList, element.Name)
+	}
+	return cat.LaunchSynchronisationVappTemplates(nameList)
+}
+
+// LaunchSynchronisationMediaItems starts synchronisation of a list of media items
+func (cat *AdminCatalog) LaunchSynchronisationMediaItems(nameList []string) ([]*Task, error) {
+	var taskList []*Task
+	mediaList, err := cat.QueryMediaList()
+	if err != nil {
+		return nil, err
+	}
+	var actionList []string
+
+	var found = make(map[string]string)
+	for _, element := range mediaList {
+		if contains(element.Name, nameList) {
+			util.Logger.Printf("scheduling for synchronisation Media item %s with catalog item HREF %s\n", element.Name, element.CatalogItem)
+			actionList = append(actionList, element.CatalogItem)
+			found[element.Name] = element.CatalogItem
+		}
+	}
+	if len(actionList) < len(nameList) {
+		var foundList []string
+		for k := range found {
+			foundList = append(foundList, k)
+		}
+		return nil, fmt.Errorf("%d names provided [%v] but %d actions scheduled [%v]\n", len(nameList), nameList, len(actionList), foundList)
+	}
+	for _, element := range actionList {
+		util.Logger.Printf("synchronising Media catalog item HREF %s\n", element)
+		catalogItem, err := cat.GetCatalogItemByHref(element)
+		if err != nil {
+			return nil, err
+		}
+		task, err := catalogItem.LaunchSync()
+		if err != nil {
+			return nil, err
+		}
+		taskList = append(taskList, task)
+	}
+	return taskList, nil
+}
+
+// LaunchSynchronisationAllMediaItems starts synchronisation of all media items for a given catalog
+func (cat *AdminCatalog) LaunchSynchronisationAllMediaItems() ([]*Task, error) {
+	var taskList []*Task
+	mediaList, err := cat.QueryMediaList()
+	if err != nil {
+		return nil, err
+	}
+	for _, element := range mediaList {
+		catalogItem, err := cat.GetCatalogItemByHref(element.CatalogItem)
+		if err != nil {
+			return nil, err
+		}
+		task, err := catalogItem.LaunchSync()
+		if err != nil {
+			return nil, err
+		}
+		taskList = append(taskList, task)
+	}
+	return taskList, nil
+}
+
+// GetCatalogItemByHref finds a CatalogItem by HREF
+// On success, returns a pointer to the CatalogItem structure and a nil error
+// On failure, returns a nil pointer and an error
+func (cat *AdminCatalog) GetCatalogItemByHref(catalogItemHref string) (*CatalogItem, error) {
+	catItem := NewCatalogItem(cat.client)
+
+	_, err := cat.client.ExecuteRequest(catalogItemHref, http.MethodGet,
+		"", "error retrieving catalog item: %s", nil, catItem.CatalogItem)
+	if err != nil {
+		return nil, err
+	}
+	return catItem, nil
 }
