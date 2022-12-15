55df4f95336da0204aeb68defde6aadacc44501e
Dainius Serplis <dserplis@vmware.com>
2022-12-15 21:25:15 +0200

    Add 401 check in auth and tests
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- govcd/api_vcd.go
+++ govcd/api_vcd.go
@@ -92,6 +92,14 @@ func (vcdClient *VCDClient) vcdCloudApiAuthorize(user, pass, org string) (*http.
 	}
 
 	defer resp.Body.Close()
+
+	// Catch HTTP 401 (Status Unauthorized) to return an error as otherwise this library will return
+	// odd errors while doing lookup of resources and confused user.
+	if resp.StatusCode == http.StatusUnauthorized {
+		return nil, fmt.Errorf("received response HTTP %d (Unauthorized). Please check if your credentials are valid",
+			resp.StatusCode)
+	}
+
 	// Store the authorization header
 	vcdClient.Client.VCDToken = resp.Header.Get(BearerTokenHeader)
 	vcdClient.Client.VCDAuthHeader = BearerTokenHeader
