58f8c310c5761dc68e294f2e7e63135f1b6aebdf
Adam Barreiro <adambarreiro@users.noreply.github.com>
2023-03-10 12:34:00 +0100

    Add CRUD methods that support Runtime Defined Entity (RDE) Types (#545)
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- types/v56/constants.go
+++ types/v56/constants.go
@@ -390,6 +390,7 @@ const (
 	OpenApiEndpointEdgeBgpConfigPrefixLists           = "edgeGateways/%s/routing/bgp/prefixLists/" // '%s' is NSX-T Edge Gateway ID
 	OpenApiEndpointEdgeBgpConfig                      = "edgeGateways/%s/routing/bgp"              // '%s' is NSX-T Edge Gateway ID
 	OpenApiEndpointRdeInterfaces                      = "interfaces/"
+	OpenApiEndpointRdeEntityTypes                     = "entityTypes/"
 
 	// NSX-T ALB related endpoints
 
