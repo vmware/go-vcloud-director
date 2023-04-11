eed2f1b92fd5aed2294c0893b5f92b3146881b9b
Dainius Serplis <dserplis@vmware.com>
2023-04-11 08:48:12 +0300

    Add missing build tags to test file
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- govcd/importable_dvpg_test.go
+++ govcd/importable_dvpg_test.go
@@ -1,3 +1,9 @@
+//go:build network || nsxt || functional || openapi || ALL
+
+/*
+ * Copyright 2023 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
+ */
+
 package govcd
 
 import (
