5d801ce40c026aa1c0d1a9b6c7401fd080f5a8fb
Giuseppe Maxia <gmaxia@vmware.com>
2023-06-07 15:13:09 +0200

    Improve SAML CRUD functions
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- govcd/org_saml.go
+++ govcd/org_saml.go
@@ -10,6 +10,8 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
+	"github.com/vmware/go-vcloud-director/v2/util"
+	"io"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -60,11 +62,6 @@ func (adminOrg *AdminOrg) SetFederationSettings(settings *types.OrgFederationSet
 		return nil, err
 	}
 
-	settings.SAMLMetadata, err = normalizeSamlMetadata(settings.SAMLMetadata)
-	if err != nil {
-		return nil, fmt.Errorf("error normalising SAML metadata: %s", err)
-	}
-
 	text := bytes.Buffer{}
 	encoder := json.NewEncoder(&text)
 	encoder.SetEscapeHTML(false)
@@ -88,6 +85,20 @@ func (adminOrg *AdminOrg) SetFederationSettings(settings *types.OrgFederationSet
 		return nil, err
 	}
 
+	if !isSuccessStatus(resp.StatusCode) {
+		body, _ := io.ReadAll(resp.Body)
+		var jsonError types.OpenApiError
+		err = json.Unmarshal(body, &jsonError)
+		// By default, we return the whole response body as error message. This may also contain the stack trace
+		message := string(body)
+		// if the body contains a valid JSON representation of the error, we return a more agile message, using the
+		// exposed fields, and hiding the stack trace from view
+		if err == nil {
+			message = fmt.Sprintf("%s - %s", jsonError.MinorErrorCode, jsonError.Message)
+		}
+		return nil, fmt.Errorf("error setting SAML for org %s: %s (%d) - %s", adminOrg.AdminOrg.Name, resp.Status, resp.StatusCode, message)
+	}
+
 	_, err = checkResp(resp, err)
 	if err != nil {
 		return nil, err
@@ -96,32 +107,54 @@ func (adminOrg *AdminOrg) SetFederationSettings(settings *types.OrgFederationSet
 	return &newSettings, nil
 }
 
-func (adminOrg *AdminOrg) GetSamlMetadata() (*types.VcdSamlMetadata, error) {
-
+// UnsetFederationSettings removes federation (SAML) settings for a given organization
+func (adminOrg *AdminOrg) UnsetFederationSettings() error {
 	settings, err := adminOrg.GetFederationSettings()
 	if err != nil {
-		return nil, err
-	}
-	metadataUrl := getUrlFromLink(settings.Link, "down", types.MimeSamlMetadata)
-	if metadataUrl == "" {
-		return nil, fmt.Errorf("no URL found for metadata retrieval (%s) in org %s", types.MimeSamlMetadata, adminOrg.AdminOrg.Name)
+		return fmt.Errorf("[UnsetFederationSettings] error getting SAML settings for Org %s: %s", adminOrg.AdminOrg.Name, err)
 	}
 
+	settings.SAMLMetadata = ""
+	settings.Enabled = false
+	settings.SamlSPEntityID = ""
+	_, err = adminOrg.SetFederationSettings(settings)
+	return err
+}
+
+// GetServiceProviderSamlMetadata retrieves the service provider SAML metadata of the given Org
+func (adminOrg *AdminOrg) GetServiceProviderSamlMetadata() (*types.VcdSamlMetadata, error) {
+
+	metadataText, err := adminOrg.RetrieveServiceProviderSamlMetadata()
+	if err != nil {
+		return nil, err
+	}
 	var metadata types.VcdSamlMetadata
 
-	resp, err := adminOrg.client.ExecuteRequest(metadataUrl, http.MethodGet, types.MimeSamlMetadata,
-		"error getting metadata: %s", nil, &metadata)
-
+	err = xml.Unmarshal([]byte(metadataText), &metadata)
 	if err != nil {
-		return nil, err
-	}
-
-	_, err = checkResp(resp, err)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("[GetSamlMetadata] error decoding metadata retrieved from %s: %s", adminOrg.AdminOrg.Name, err)
 	}
 
 	return &metadata, nil
+}
+
+// RetrieveServiceProviderSamlMetadata retrieves the SAML metadata of the given Org
+func (adminOrg *AdminOrg) RetrieveServiceProviderSamlMetadata() (string, error) {
+
+	settings, err := adminOrg.GetFederationSettings()
+	if err != nil {
+		return "", err
+	}
+	metadataUrl := getUrlFromLink(settings.Link, "down", types.MimeSamlMetadata)
+	if metadataUrl == "" {
+		return "", fmt.Errorf("[RetrieveRemoteDocument] no URL found for metadata retrieval (%s) in org %s", types.MimeSamlMetadata, adminOrg.AdminOrg.Name)
+	}
+
+	metadataText, err := adminOrg.client.RetrieveRemoteDocument(metadataUrl)
+	if err != nil {
+		return "", fmt.Errorf("[RetrieveRemoteDocument] error retrieving SAML metadata from %s: %s", metadataUrl, err)
+	}
+	return string(metadataText), nil
 }
 
 func getUrlFromLink(linkList types.LinkList, wantRel, wantType string) string {
@@ -154,9 +187,33 @@ var (
 	}
 )
 
-// normalizeSamlMetadata takes a string containing the XML code with Metadata definition
+// RetrieveRemoteDocument gets the contents of a given URL
+func (client *Client) RetrieveRemoteDocument(metadataUrl string) ([]byte, error) {
+
+	retrieveUrl, err := url.Parse(metadataUrl)
+	if err != nil {
+		return nil, err
+	}
+	request := client.newRequest(nil, nil, http.MethodGet, *retrieveUrl, nil, client.APIVersion, nil)
+
+	resp, err := client.Http.Do(request)
+
+	if err != nil {
+		return nil, fmt.Errorf("[RetrieveRemoteDocument] error retrieving metadata from %s: %s", metadataUrl, err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("[RetrieveRemoteDocument] error reading response body from metadata retrieved from %s: %s", metadataUrl, err)
+	}
+
+	util.ProcessResponseOutput("[RetrieveRemoteDocument]", resp, string(body))
+	return body, nil
+}
+
+// normalizeServiceProviderSamlMetadata takes a string containing the XML code with Metadata definition
 // and makes sure it has all the expected elements
-func normalizeSamlMetadata(in string) (string, error) {
+func normalizeServiceProviderSamlMetadata(in string) (string, error) {
 	var metadata types.VcdSamlMetadata
 
 	// Phase 1: Decode the XML, to find possible encoding errors
@@ -193,14 +250,10 @@ func normalizeSamlMetadata(in string) (string, error) {
 		}
 	}
 
-	// Phase 5: Add the XML header if it is missing
-	if !strings.Contains(metadataText, types.XmlHeader) {
-		metadataText = types.XmlHeader + metadataText
-	}
-
 	return metadataText, nil
 }
 
+// validateNamespaceDefinition checks that a metadata XML text contains the expected namespace definition
 func validateNamespaceDefinition(metadataText string, namespace string) bool {
 	reEmptyDefinition := regexp.MustCompile(`xmlns:` + namespace + `\s*=\s*""`)
 	reFilledDefinition := regexp.MustCompile(`xmlns:` + namespace + `\s*=\s*"\S+"`)
@@ -219,9 +272,9 @@ func validateNamespaceDefinition(metadataText string, namespace string) bool {
 	return found != ""
 }
 
-// ValidateSamlMetadata tells whether a given string contains valid XML that defines SAML metadata
+// ValidateSamlServiceProviderMetadata tells whether a given string contains valid XML that defines SAML service provider metadata
 // Returns nil on valid data, and an array of errors for invalid data
-func ValidateSamlMetadata(metadataText string) []error {
+func ValidateSamlServiceProviderMetadata(metadataText string) []error {
 	var metadata types.VcdSamlMetadata
 	var errors []error
 
@@ -231,26 +284,27 @@ func ValidateSamlMetadata(metadataText string) []error {
 		errors = append(errors, fmt.Errorf("[ValidateSamlMetadata] error decoding XML into SAML metadata structure: %s", err))
 	}
 
-	// Check n. 2: make sure the input contains a XML header
-	if !strings.Contains(metadataText, types.XmlHeader) {
-		errors = append(errors, fmt.Errorf("[ValidateSamlMetadata] SAML metadata does not include a XML header"))
+	reNameSpace, err := regexp.Compile(`<(\w+):(\w+)`)
+
+	if err != nil {
+		errors = append(errors, fmt.Errorf("error compiling regular expression: %s", err))
+		return errors
 	}
 
-	// Check n. 3: make sure the namespaces are defined
-	for _, ns := range []string{"md", "ds", "hoksso"} {
+	nsInfoList := reNameSpace.FindAllStringSubmatch(metadataText, -1)
+	processed := map[string]bool{}
+
+	// Check n. 2: make sure that each namespace used in the metadata text has a corresponding definition
+	for _, nsInfo := range nsInfoList {
+		seen, ok := processed[nsInfo[0]]
+		if ok && seen {
+			continue
+		}
+		ns := nsInfo[1]
 		if !validateNamespaceDefinition(metadataText, ns) {
 			errors = append(errors, fmt.Errorf("[ValidateSamlMetadata] namespace '%s' undefined in SAML metadata", ns))
 		}
-	}
-
-	// Check n. 4: make sure the expected fields have their namespace included
-	for ns, fields := range samlMetadataItems {
-		for _, fieldName := range fields {
-			fullName := fmt.Sprintf("%s:%s", ns, fieldName)
-			if !strings.Contains(metadataText, fullName) {
-				errors = append(errors, fmt.Errorf("[ValidateSamlMetadata] field %s not found in SAML metadata", fullName))
-			}
-		}
+		processed[nsInfo[0]] = true
 	}
 
 	if len(errors) == 0 {
@@ -259,6 +313,7 @@ func ValidateSamlMetadata(metadataText string) []error {
 	return errors
 }
 
+// GetErrorMessageFromErrorSlice returns a single error message from a list of error
 func GetErrorMessageFromErrorSlice(errors []error) string {
 	result := ""
 	for i, err := range errors {
