c2d86aa99e0708f2c3bdaf8c84de53637a670c09
abarreiro <abarreiro@vmware.com>
2023-06-21 12:23:30 +0200

    Init
    Signed-off-by: abarreiro <abarreiro@vmware.com>

---
+++ govcd/defined_interface_behavior.go
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2023 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
+ */
+
+package govcd
+
+// DefinedInterfaceBehavior is a type for handling Defined Interface Behaviors, from the Runtime Defined Entities framework, in VCD.
+//type DefinedInterfaceBehavior struct {
+//	//client *Client
+//}
