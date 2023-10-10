493c8cf6737540b0b2797ada66f0879ccd0ba997
Adam Jasinski <jasinskia@vmware.com>
2023-10-10 08:52:10 +0300

    Fix getting by name
    Signed-off-by: Adam Jasinski <jasinskia@vmware.com>

--- govcd/nsxt_l2_vpn_tunnel.go
+++ govcd/nsxt_l2_vpn_tunnel.go
@@ -128,27 +128,14 @@ func (egw *NsxtEdgeGateway) GetL2VpnTunnelByName(name string) (*NsxtL2VpnTunnel,
 		return nil, err
 	}
 
-	var found bool
-	var foundTunnel *NsxtL2VpnTunnel
+	foundTunnels := make([]*NsxtL2VpnTunnel, 0)
 	for _, tunnel := range results {
 		if tunnel.NsxtL2VpnTunnel.Name == name {
-			// There can be more than one SERVER tunnel with the same name,
-			// return an error if more than two are found
-			if !found {
-				found = true
-				foundTunnel = tunnel
-			}
-			if found {
-				return nil, fmt.Errorf("two tunnels with the name %s found", name)
-			}
+			foundTunnels = append(foundTunnels, tunnel)
 		}
 	}
 
-	if foundTunnel != nil {
-		return foundTunnel, nil
-	}
-
-	return nil, fmt.Errorf("tunnel with the name %s not found", name)
+	return oneOrError("name", name, foundTunnels)
 }
 
 // GetL2VpnTunnelById gets the L2 VPN Tunnel by its ID
