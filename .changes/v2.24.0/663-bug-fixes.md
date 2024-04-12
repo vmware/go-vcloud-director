f2d7137089f68b390a654bc76e4a27752b2f6350
abarreiro <abarreiro@vmware.com>
2024-04-12 17:52:56 +0200

    Fix mispelling
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- .changes/v2.24.0/663-bug-fixes.md
+++ .changes/v2.24.0/663-bug-fixes.md
@@ -1,4 +1,4 @@
-* Fixed an issue that prevented CSE Kubernetes clusters to be upgraded to an OVA with higher Kubernetes version but same TKG version,
+* Fixed an issue that prevented CSE Kubernetes clusters from being upgraded to an OVA with higher Kubernetes version but same TKG version,
   and to an OVA with a higher patch version of Kubernetes [GH-663] 
-* Fixed an issue that prevented CSE Kubernetes clusters to be upgraded to TKG v2.5.0 with Kubernetes v1.26.11 as it
+* Fixed an issue that prevented CSE Kubernetes clusters from being upgraded to TKG v2.5.0 with Kubernetes v1.26.11 as it
   performed an invalid upgrade of CoreDNS [GH-663] 
--- govcd/cse.go
+++ govcd/cse.go
@@ -233,7 +233,7 @@ func (cluster *CseKubernetesCluster) GetSupportedUpgrades(refreshOvas bool) ([]*
 		if err != nil {
 			continue // This means it's not a TKGm OVA, or it is not supported, so we skip it
 		}
-		// The OVA can be used if the TKG version is higher or equal than the actual and the Kubernetes version is at most 1 minor higher.
+		// The OVA can be used if the TKG version is equal to the actual or higher, and the Kubernetes version is at most 1 minor higher.
 		if targetVersions.compareTkgVersion(cluster.TkgVersion.String()) >= 0 && targetVersions.kubernetesVersionIsUpgradeableFrom(cluster.KubernetesVersion.String()) {
 			cluster.supportedUpgrades = append(cluster.supportedUpgrades, vAppTemplate.VAppTemplate)
 		}
