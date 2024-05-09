e8a6f5472ef7820f814de4ccbe1b741c26984299
Josafá Soares de Souza Júnior <josafa.souza@serpro.gov.br>
2024-05-09 17:08:41 -0300

    Bucket: Create + Get + Delete + Edit tags

--- govcd/object_storage_ext.go
+++ govcd/object_storage_ext.go
@@ -2,8 +2,10 @@ package govcd
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -16,7 +18,11 @@ import (
 // the last bit for s3 API.
 // Sample URL construct: https://s3.HOST//api/v1/s3
 func (client *Client) S3ApiBuildEndpoint(endpoint ...string) (*url.URL, error) {
-	endpointString := client.VCDHREF.Scheme + "://s3." + client.VCDHREF.Host + "/api/v1/s3/" + strings.Join(endpoint, "/")
+	endpointString := client.VCDHREF.Scheme + "://s3." + client.VCDHREF.Host + "/api/v1/s3"
+	if endpoint != nil {
+		endpointString = endpointString + "/" + strings.Join(endpoint, "/")
+	}
+
 	urlRef, err := url.ParseRequestURI(endpointString)
 	if err != nil {
 		return nil, fmt.Errorf("error formatting S3API endpoint: %s", err)
@@ -56,7 +62,7 @@ func (client *Client) newS3ApiRequest(apiVersion string, params url.Values, meth
 		// The deprecated authorization token is 32 characters long
 		// The bearer token is 612 characters long
 		if len(client.VCDToken) > 32 {
-			req.Header.Add("Authorization", "bearer "+client.VCDToken)
+			req.Header.Add("Authorization", "Bearer "+client.VCDToken)
 			req.Header.Add("X-Vmware-Vcloud-Token-Type", "Bearer")
 		}
 		// Add the Accept header for VCD
@@ -94,13 +100,125 @@ func (client *Client) newS3ApiRequest(apiVersion string, params url.Values, meth
 	return req
 }
 
-func (client *Client) S3ApiCreateBucket(name string, additionalHeader map[string]string) (*http.Response, error) {
-	url, _ := client.S3ApiBuildEndpoint(name)
+func (client *Client) S3ApiGetBuckets(region string, additionalHeader map[string]string) (string, error) {
+	urlRef, _ := client.S3ApiBuildEndpoint()
 
-	req := client.newS3ApiRequest(client.APIVersion, nil, http.MethodPut, url, nil, nil)
+	values, _ := url.ParseQuery("offset=0&limit=1000")
+
+	req := client.newS3ApiRequest(client.APIVersion, values, http.MethodGet, urlRef, nil, nil)
 
 	resp, err := client.Http.Do(req)
 	if err != nil {
+		util.Logger.Printf("[DEBUG - S3ApiCreateBucket] error creating: %s", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("[DEBUG - S3ApiGetBuckets] Error reading HTTP response body: %s", err)
+			return "", err
+		}
+		return string(body), nil
+	} else {
+		return "", ErrorEntityNotFound
+	}
+}
+
+func (client *Client) S3ApiGetBucket(name, region string, additionalHeader map[string]string) (string, error) {
+	urlRef, _ := client.S3ApiBuildEndpoint(name)
+
+	req := client.newS3ApiRequest(client.APIVersion, nil, http.MethodGet, urlRef, nil, nil)
+
+	resp, err := client.Http.Do(req)
+	if err != nil {
+		util.Logger.Printf("[DEBUG - S3ApiCreateBucket] error creating: %s", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("[DEBUG - S3ApiGetBuckets] Error reading HTTP response body: %s", err)
+			return "", err
+		}
+		return string(body), nil
+	} else {
+		return "", ErrorEntityNotFound
+	}
+}
+
+func (client *Client) S3ApiCreateBucket(name, region string, additionalHeader map[string]string) (*http.Response, error) {
+	urlRef, _ := client.S3ApiBuildEndpoint(name)
+
+	body := fmt.Sprintf(`{"name":"%s", "locationConstraint":"%s"}`, name, region)
+
+	req := client.newS3ApiRequest(client.APIVersion, nil, http.MethodPut, urlRef, bytes.NewBuffer([]byte(body)), nil)
+
+	resp, err := client.Http.Do(req)
+	if err != nil {
+		util.Logger.Printf("[DEBUG - S3ApiCreateBucket] error creating: %s", err)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (client *Client) S3ApiDeleteBucket(name, region string, additionalHeader map[string]string) (*http.Response, error) {
+	urlRef, _ := client.S3ApiBuildEndpoint(name)
+
+	body := `{
+		"quiet": true,
+		"removeAll": true,
+		"deleteVersion": true,
+		"tryAsync": true
+	}`
+
+	values, err := url.ParseQuery("delete")
+	if err != nil {
+		util.Logger.Printf("[DEBUG - ParseQuery] error ParseQuery bucket: %s", err)
+		return nil, err
+	}
+
+	req := client.newS3ApiRequest(client.APIVersion, values, http.MethodDelete, urlRef, bytes.NewBuffer([]byte(body)), nil)
+
+	resp, err := client.Http.Do(req)
+	if err != nil {
+		util.Logger.Printf("[DEBUG - S3ApiDeleteBucket] error deleting bucket: %s", err)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (client *Client) S3ApiEditBucketTags(name, region string, tags map[string]string, additionalHeader map[string]string) (*http.Response, error) {
+	urlRef, _ := client.S3ApiBuildEndpoint(name)
+
+	values, _ := url.ParseQuery("tagging")
+
+	tagSet := make(map[string][]interface{})
+	for k, v := range tags {
+		tag := make(map[string]interface{})
+		tag["key"] = k
+		tag["value"] = v
+		tagSet["tags"] = append(tagSet["tags"], tag)
+	}
+
+	body := make(map[string][]interface{})
+	body["tagSets"] = append(body["tagSet"], tagSet)
+
+	data, err := json.Marshal(body)
+	if err != nil {
+		util.Logger.Printf("Error marshaling json %s", err)
+	}
+
+	req := client.newS3ApiRequest(client.APIVersion, values, http.MethodPut, urlRef, bytes.NewBuffer(data), nil)
+
+	resp, err := client.Http.Do(req)
+	if err != nil {
+		util.Logger.Printf("[DEBUG - S3ApiEditBucketTags] error editing bucket tags: %s", err)
 		return nil, err
 	}
 
