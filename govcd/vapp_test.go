9423b82d7edee0888485cc06d653f9d2d6656099
abarreiro <abarreiro@vmware.com>
2024-06-13 16:12:01 +0200

    #
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- govcd/vapp_test.go
+++ govcd/vapp_test.go
@@ -958,7 +958,7 @@ func (vcd *TestVCD) Test_AddAndRemoveIsolatedVappNetworkIpv6(check *C) {
 
 	check.Assert(networkFound.Description, Equals, description)
 	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].Gateway, Equals, gateway)
-	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].SubnetPrefixLengthString, Equals, prefixlength)
+	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].SubnetPrefixLength, Equals, prefixlength)
 	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].DNS1, Equals, dns1)
 	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].DNS2, Equals, dns2)
 	check.Assert(networkFound.Configuration.IPScopes.IPScope[0].DNSSuffix, Equals, dnsSuffix)
