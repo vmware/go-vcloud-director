1d72ea21f72b928b75c6d1bf2b63f4342baf9f69
abarreiro <abarreiro@vmware.com>
2024-11-08 12:42:53 +0100

    #
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- govcd/tm_common_test.go
+++ govcd/tm_common_test.go
@@ -1,4 +1,4 @@
-//go:build tm || functional || ALL
+//go:build api || openapi || functional || catalog || vapp || gateway || network || org || query || extnetwork || task || vm || vdc || system || disk || lb || lbAppRule || lbAppProfile || lbServerPool || lbServiceMonitor || lbVirtualServer || user || search || nsxv || nsxt || auth || affinity || role || alb || certificate || vdcGroup || metadata || providervdc || rde || vsphere || uiPlugin || cse || slz || tm || ALL
 
 /*
  * Copyright 2024 VMware, Inc.  All rights reserved.  Licensed under the Apache v2 License.
