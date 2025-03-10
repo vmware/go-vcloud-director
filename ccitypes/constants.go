dfad2ca47c3eaa6d32ea009616a420a444e18128
Ferran Rodenas <ferran.rodenas@broadcom.com>
2025-03-10 10:48:23 -0700

    Update CCI types to v1alpha2
    * Update `Project` version to `v1alpha2`
    * Update `SupervisorNamespace` version to `v1alpha2`
    * Update `SupervisorNamespace` proprties
    
    Signed-off-by: Ferran Rodenas <ferran.rodenas@broadcom.com>

--- ccitypes/constants.go
+++ ccitypes/constants.go
@@ -3,12 +3,12 @@ package ccitypes
 const (
 	SupervisorNamespaceKind    = "SupervisorNamespace"
 	SupervisorNamespaceAPI     = "infrastructure.cci.vmware.com"
-	SupervisorNamespaceVersion = "v1alpha1"
+	SupervisorNamespaceVersion = "v1alpha2"
 	SupervisorNamespacesURL    = "/apis/" + SupervisorNamespaceAPI + "/" + SupervisorNamespaceVersion + "/namespaces/%s/supervisornamespaces"
 
 	ProjectAPI     = "project.cci.vmware.com"
 	ProjectKind    = "Project"
-	ProjectVersion = "v1alpha1"
+	ProjectVersion = "v1alpha2"
 	ProjectsURL    = "/apis/" + ProjectAPI + "/" + ProjectVersion + "/projects"
 
 	KubernetesSubpath = "%s://%s/cci/kubernetes"
