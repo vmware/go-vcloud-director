458a0ecd46633929f305d8b519be2371a574fdd3
Dainius Serplis <dserplis@vmware.com>
2025-03-19 13:50:43 +0200

    Self review
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- govcd/feture_flag.go
+++ govcd/feture_flag.go
@@ -10,15 +10,6 @@ import (
 	"github.com/vmware/go-vcloud-director/v3/types/v56"
 )
 
-// {
-// 	"id": "urn:vcloud:featureflag:CLASSIC_TENANT_CREATION",
-// 	"name": "CLASSIC_TENANT_CREATION",
-// 	"usage": "ALPHA",
-// 	"enabled": true,
-// 	"displayName": "Classic Tenant Creation",
-// 	"displayDescription": "This feature enables the creation of classic tenant Organizations."
-//   }
-
 const labelFeatureFlag = "Org VDC Network Segment Profile"
 
 func (vcdClient *VCDClient) UpdateFeatureFlag(entityConfig *types.FeatureFlag) (*types.FeatureFlag, error) {
@@ -48,7 +39,6 @@ func (vcdClient *VCDClient) GetFeatureFlagById(featureFlagId string) (*types.Fea
 }
 
 func (vcdClient *VCDClient) GetAllFeatureFlags() ([]*types.FeatureFlag, error) {
-
 	c := crudConfig{
 		endpoint:    types.OpenApiPathVersion1_0_0 + types.OpenApiEndpointFeatureFlags,
 		entityLabel: labelFeatureFlag,
